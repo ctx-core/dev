d5e988167ef237c6e566fa9c6fe66986cbc4c5fe
Brian Takita <brian.takita@gmail.com>
2016-06-04 18:49:22 -0400

    layoutHtml fn$head$html

--- layout/layout.html.js
+++ layout/layout.html.js
@@ -5,13 +5,15 @@ import {log,debug} from "ctx-core/logger/lib"
 const logPrefix = "ctx-core/layout.html";
 export default function layoutHtml() {
   log(`${logPrefix}|layoutHtml`);
-  const ctx = clone(...arguments);
+  const ctx = clone(...arguments)
+      , fn$head$html = (ctx.fn$head$html || function() {return "";});
   return `
     <html>
       <head>
         <title>${ctx.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${css$html(ctx, {indentation: fn$indentation(4), indentFirstLine: false})}
+        ${fn$head$html()}
       </head>
       ${ctx.body$html}
     </html>`.replace(indentation$regexp(4), "");
