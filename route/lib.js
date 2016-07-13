6a2957199ba63069430e6e26ac1a60bd867ac851
Brian Takita <brian.takita@gmail.com>
2016-07-12 20:40:23 -0400

    new__route__query$$ from new__route$with_query$$

--- route/lib.js
+++ route/lib.js
@@ -36,8 +36,8 @@ export function assign__agent__route$name() {
   });
   return ctx;
 }
-export function new__route$with_query$$(ctx, ...opts$ctx$$) {
-  log(`${logPrefix}|new__route$with_query$$`);
+export function new__route__query$$(ctx, ...opts$ctx$$) {
+  log(`${logPrefix}|new__route__query$$`);
   const opts$ctx = clone(...opts$ctx$$)
       , new__route$ = opts$ctx.new__route || new__route
       , path = opts$ctx.path;
