170d846476ba391fa4230d0419cb79677fe2acc0
Brian Takita <brian.takita@gmail.com>
2016-09-19 07:09:51 -0400

    html/lib.js
    * jsdocs
    * compose$list__new__html$ctx from assign__new__html$ctx
    * compose$list__new__html$ctx
    * html__js from html_js

--- html/lib.js
+++ html/lib.js
@@ -1,13 +1,38 @@
+/**
+ * @module ctx-core/html/lib
+ */
 import {assign} from 'ctx-core/object/lib'
 import {concat__array} from 'ctx-core/array/lib'
 import env from 'ctx-core/env'
 import {log,debug} from 'ctx-core/logger/lib'
 const logPrefix = 'ctx-core/html/lib'
 let new__html$ctx__store = []
-export function assign__new__html$ctx() {
-  log(`${logPrefix}|assign__new__html$ctx`)
-  new__html$ctx__store.push(...arguments)
+/**
+ * Returns a new html$ctx
+ * @typedef {function} new__html$ctx
+ * @returns {module:ctx-core/html/lib~html$ctx}
+ */
+/**
+ * push new__html$ctx$$ functions onto env
+ * @param {...module:ctx-core/html/lib#new__html$ctx} return values compose html$ctx
+ */
+export function compose__new__html$ctx(...new__html$ctx$$) {
+  log(`${logPrefix}|compose__new__html$ctx`)
+  compose$list__new__html$ctx().push(...new__html$ctx$$)
+  return env
 }
+export function compose$list__new__html$ctx() {
+  log(`${logPrefix}|compose$list__new__html$ctx`)
+  let {compose$list__new__html$ctx = []} = env
+  env.compose$list__new__html$ctx = compose$list__new__html$ctx
+  return compose$list__new__html$ctx
+}
+/**
+ * Returns a new html$ctx
+ * @param ctx
+ * @param html$ctx$$
+ * @returns {{}}
+ */
 export function new__html$ctx(ctx, ...html$ctx$$) {
   log(`${logPrefix}|new__html$ctx`)
   let html$ctx = {}
@@ -19,6 +44,10 @@ export function new__html$ctx(ctx, ...html$ctx$$) {
   assign(html$ctx, ...html$ctx$$)
   return html$ctx
 }
+/**
+ * html for css link tags
+ * @returns {string}
+ */
 export function html_css() {
   log(`${logPrefix}|html_css`)
   const ctx = assign({
@@ -31,13 +60,21 @@ export function html_css() {
     return `${(i || indentFirstLine) ? indentation : ''}<link rel="stylesheet" type="text/css" href="${cssFile}">`
   }).join('\n')
 }
+/**
+ * versioned css file url
+ * @param script$src
+ */
 export function css$versioned(script$src) {
   log(`${logPrefix}|js$versioned`)
   const extName = '.css'
   return versioned(`${script$src}${extName}`)
 }
-export function html_js() {
-  log(`${logPrefix}|html_js`)
+/**
+ * html for js script tags
+ * @returns {string}
+ */
+export function html__js() {
+  log(`${logPrefix}|html__js`)
   const ctx = assign({js: [], indentation: '', indentFirstLine: true}, ...arguments)
       , {js, indentation, indentFirstLine} = ctx
   return concat__array([],
@@ -49,15 +86,29 @@ export function html_js() {
     `${indentation}</script>`
   ).join('\n')
 }
+/**
+ * @deprecated
+ * @type {html__js}
+ */
+export const html_js = html__js
 export function js$versioned(script$src) {
   log(`${logPrefix}|js$versioned`)
   const extName = env.minify ? '.min.js' : '.js'
   return versioned(`${script$src}${extName}`)
 }
-export function versioned(script$src) {
+/**
+ * versioned file
+ * @param {string} url
+ * @returns {string}
+ */
+export function versioned(url) {
   log(`${logPrefix}|versioned`)
-  return `${script$src}?${version$query()}`
+  return `${url}?${version$query()}`
 }
+/**
+ * version query param
+ * @returns {string}
+ */
 export function version$query() {
   return `v=${encodeURIComponent(env.CACHE_VERSION)}`
 }
