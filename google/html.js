0913dc1cd11bead33bd958907110ee90b9706b28
Brian Takita <brian.takita@gmail.com>
2016-11-30 17:11:04 -0500

    google/html.js
    * clone
    * formatting
    * $script__google__analytics
      * env.GA_ID
    
    package.json.sample
    
    * desciption
      * This is a kitchen-sink repo dependency-laden package.json
    * "atob-lite": "^1.0.0"
    * "aws-sdk": "2.7.8"
    * "babel-traverse": "6.19.0"
    * "babel-types": "6.19.0"
    * "fs-promise": "^1.0.0"
    * "graceful-fs": "^4.1.11"
    * "inquirer": "^1.2.3"
    * "inquirer-autocomplete-prompt": "github:vweevers/inquirer-autocomplete-prompt#inquirer-0.11
    * "jsdoc": "^3.4.3"
    * "koa-bodyparser": "^2.3.0"
    * "minimist": "^1.2.0"
    * "mz": "^2.6.0"
    * "newrelic": "^1.34.0"
    * "node-sass": "^3.13.0"
    * "ramda": "^0.22.1"
    * "riot": "3.0.1"
    * "riot-compiler": "3.0.0"
    * "riot-route": "^3.0.1"
    * "rollup": "0.36.4"
    * "rollup-plugin-includepaths": "^0.2.1"
    * "rollup-plugin-riot": "^1.0.0"
    * "uglify-js": "^2.7.5"
    * "vorpal": "^1.11.4"
    * "vorpal-use": "^1.0.1"
    * "whatwg-fetch": "^2.0.1"

--- google/html.js
+++ google/html.js
@@ -3,7 +3,7 @@
  * @see {@link https://developers.google.com/tag-manager}
  */
 import env from 'ctx-core/env'
-import {$indentation$regexp} from 'ctx-core/string/indendation'
+import {clone} from 'ctx-core/object/lib'
 import {throw__missing_argument} from 'ctx-core/error/lib'
 import {log,debug} from 'ctx-core/logger/lib'
 const logPrefix = 'ctx-core/google/html'
@@ -14,24 +14,23 @@ const logPrefix = 'ctx-core/google/html'
  */
 export function $script__google__analytics(ctx) {
   log(`${logPrefix}|$script__google__analytics`)
-  const {GTM_ID} = env
-  if (!GTM_ID) throw__missing_argument(ctx, {key: 'env.GTM_ID'})
+  const {GA_ID} = env
+  if (!GA_ID) throw__missing_argument(ctx, {key: 'env.GA_ID'})
   return `
-    <!-- Google Analytics -->
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
-    ga('create', '${GTM_ID}', 'auto');  // Replace with your property ID.
-    ga('send', 'pageview');
-    
-    </script>
-    <!-- End Google Analytics -->`
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');  
+ga('create', '${GA_ID}', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
+  `.trim()
 }
 /**
- * Google Tag Manager script html to place at the top of <head>
+ * Google Tag Manager script html to place at the top of `<head>`
  * @param {module:ctx-core/object/lib~ctx}
  * @returns {string} html
  */
@@ -42,18 +41,18 @@ export function $script__head__gtm(ctx, ...opts$$) {
       , {dataLayer=[]} = opts
   if (!GTM_ID) throw__missing_argument(ctx, {key: 'env.GTM_ID'})
   return `
-    <script>window.dataLayer = ${JSON.stringify(dataLayer)};</script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${GTM_ID}');</script>
-    <!-- End Google Tag Manager -->
-  `.trim().replace($indentation$regexp(0), '')
+<script>window.dataLayer = ${JSON.stringify(dataLayer)};</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');</script>
+<!-- End Google Tag Manager -->
+  `.trim()
 }
 /**
- * Google Tag Manager script html to place at the top of <body>
+ * Google Tag Manager script html to place at the top of `<body>`
  * @param {module:ctx-core/object/lib~ctx}
  * @returns {string} html
  */
@@ -62,9 +61,9 @@ export function $script__body__gtm(ctx) {
   const {GTM_ID} = env
   if (!GTM_ID) throw__missing_argument(ctx, {key: 'env.GTM_ID'})
   return `
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${GTM_ID}"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-  `.trim().replace($indentation$regexp(0), '')
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  `.trim()
 }
