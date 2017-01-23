ace3743eb0de06b635182a5901faabd55b481cc5
Brian Takita <brian.takita@gmail.com>
2017-01-23 09:08:56 -0500

    drip_marketing/lib.js
    * logPrefix

--- drip_marketing/lib.js
+++ drip_marketing/lib.js
@@ -1,4 +1,5 @@
 import {log,debug} from 'ctx-core/logger/lib'
+const logPrefix = 'ctx-core/drip_marketing/lib'
 export function push__drip() {
   log(`${logPrefix}|push__dcq`)
   if (typeof window._dcq !== 'undefined') {
