9ab59668c46c467ae4221bf2f89e7e586628c717
Brian Takita <brian.takita@gmail.com>
2017-02-27 17:59:18 -0500

    sass/sass-build.sh
    * npm rebuild node-sass

--- sass/sass-build.sh
+++ sass/sass-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# SEE https://github.com/sass/node-sass/issues/1579
+# TODO: Use yarn when https://github.com/yarnpkg/yarn/issues/2069 is resolved
+npm rebuild node-sass
 eval "$(ctx-core/bin/sass-cmd.js -- '&')"
 wait
