7fd4130ab21c2481040e8b1da0ebb1c87edf5eb5
Brian Takita <brian.takita@gmail.com>
2017-02-27 05:21:11 -0500

    rollup/riot.js
    + $browser__rollup__riot
    + $node__rollup__riot

--- rollup/riot.js
+++ rollup/riot.js
@@ -1,12 +1,31 @@
-var $plugins__node = require('ctx-core/rollup/lib').$plugins__node
+var $browser__rollup = require('ctx-core/rollup/lib').$browser__rollup
+  , $node__rollup = require('ctx-core/rollup/lib').$node__rollup
+  , $plugins__node = require('ctx-core/rollup/lib').$plugins__node
   , $plugins__browser = require('ctx-core/rollup/lib').$plugins__browser
   , riot__rollup = require('rollup-plugin-riot')
+require('ctx-core/riot/ecmascript-6')
 module.exports = {
+  $browser__rollup__riot,
   $plugins__browser__riot,
+  $node__rollup__riot,
   $plugins__node__riot
+}
+function $browser__rollup__riot() {
+  return $browser__rollup({
+    globals: {
+      global: 'window',
+      riot: 'riot'
+    },
+    plugins: $plugins__browser__riot()
+  }, ...arguments)
 }
 function $plugins__browser__riot() {
   return [riot__rollup(), ...$plugins__browser(), ...arguments]
+}
+function $node__rollup__riot() {
+  return $node__rollup({
+    plugins: $plugins__node__riot()
+  }, ...arguments)
 }
 function $plugins__node__riot() {
   return [riot__rollup(), ...$plugins__node(), ...arguments]
