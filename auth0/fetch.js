d73a14731118ca4a7cce04debf6725330a81c891
Brian Takita <brian.takita@gmail.com>
2017-03-16 15:49:40 -0400

    auth0/fetch.js
    + post__signup__dbconnections__auth0
    + post__login__auth0

--- auth0/fetch.js
+++ auth0/fetch.js
@@ -1,3 +1,4 @@
+import {assign} from 'ctx-core/object/lib'
 import {fetch} from 'ctx-core/fetch/lib'
 import {throw__missing_argument} from 'ctx-core/error/lib'
 import {log,debug} from 'ctx-core/logger/lib'
@@ -14,4 +15,32 @@ export function get__userinfo__auth0(ctx) {
       'Authorization': `Bearer ${access_token__auth0}`
     }
   })
+}
+export function post__signup__dbconnections__auth0(ctx, form) {
+  log(`${logPrefix}|fetch__signup`)
+  const body = $body(form)
+  return fetch(`https://${ctx.AUTH0_DOMAIN}/dbconnections/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body),
+  })
+}
+export function post__login__auth0(ctx, form) {
+  log(`${logPrefix}|post__login__auth0`)
+  const body = $body({grant_type: 'password'}, form)
+  return fetch(`https://${ctx.AUTH0_DOMAIN}/oauth/ro`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body),
+  })
+}
+function $body(...form) {
+  return assign({
+          client_id: ctx.AUTH0_CLIENT_ID,
+          connection: 'Username-Password-Authentication'
+        }, ...form)
 }
