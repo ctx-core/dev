79e5f0df72b2f59dcc5b0ac81e4a21299ae838a8
Brian Takita <brian.takita@gmail.com>
2017-04-07 01:21:32 -0400

    - dom-classes/lib.js
    dom/lib.js
    
    * toggle__class
      * el.classList.toggle
      * Fixed

--- dom/lib.js
+++ dom/lib.js
@@ -315,22 +315,22 @@ export function empty__window$location() {
     history.replaceState({}, '', window.location.href.slice(0, -1))
   }
 }
-export function set__class(el, class$, value) {
-  let op = value ? 'add' : 'remove'
-  return el.classList[op](class$)
-}
-export function add__class(el, class$) {
-  return el.classList.add(class$)
-}
-export function contains__class(el, class$) {
-  return el.classList.contains(class$)
+export function contains__class(el, class_name) {
+  return el.classList.contains(class_name)
 }
 export const has__class = contains__class
-export function toggle__class(el, class$) {
-  return set(el, class$, !(contains(el, class$)))
+export function set__class(el, class_name, value) {
+  let op = value ? 'add' : 'remove'
+  return el.classList[op](class_name)
 }
-export function remove__class(el, class$) {
-  return el.classList.remove(class$)
+export function add__class(el, class_name) {
+  return el.classList.add(class_name)
+}
+export function toggle__class(el, class_name) {
+  return el.classList.toggle(class_name)
+}
+export function remove__class(el, class_name) {
+  return el.classList.remove(class_name)
 }
 export function check__element(el) {
   log(`${logPrefix}|check__element`)
