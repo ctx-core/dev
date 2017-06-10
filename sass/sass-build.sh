067cda026e5c21d8eda1fa19e934c85a2e8ab99b
Brian Takita <brian.takita@gmail.com>
2017-06-10 14:37:42 -0400

    rollup/browser-rollup.sh
    * pass arguments to rollup-cmd.js
    
    rollup/http-rollup.sh
    
    * pass arguments to rollup-cmd.js
    
    sass/sass-build.sh
    
    * pass arguments to sass-cmd.js

--- sass/sass-build.sh
+++ sass/sass-build.sh
@@ -2,5 +2,5 @@
 # SEE https://github.com/sass/node-sass/issues/1579
 # TODO: Use yarn when https://github.com/yarnpkg/yarn/issues/2069 is resolved
 npm rebuild node-sass
-eval "$(ctx-core/bin/sass-cmd.js -- '&')"
+eval "$(ctx-core/bin/sass-cmd.js $@ -- '&')"
 wait
