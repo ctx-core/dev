a4e4932cdde090f479a02fd75c53393a2f3ed678
Brian Takita <brian.takita@gmail.com>
2018-01-30 14:18:52 -0500

    Auth0
      * Handle 429 responses with fibonacci backoff
    
    ---
    
    auth0/koa.mjs
    
      * $email__verify
        < sleep
        < fibonacci
        * Handle 429 responses with fibonacci backoff
    
    fibonacci/lib.mjs
    
      + fibonacci
    
    sleep/lib.mjs
    
      + sleep

--- auth0/koa.mjs
+++ auth0/koa.mjs
@@ -8,6 +8,8 @@ import {get__userinfo__auth0
       , patch__user__v2__auth0} from 'ctx-core/auth0/fetch'
 import {$token__auth0
       , $credentials__client_credentials} from 'ctx-core/auth0/management'
+import {sleep} from 'ctx-core/sleep/lib'
+import {fibonacci} from 'ctx-core/fibonacci/lib'
 import {info,debug,error,log} from 'ctx-core/logger/lib'
 const logPrefix = 'ctx-core/auth0/koa.mjs'
 export default use__auth0
@@ -80,8 +82,41 @@ export async function $email__verify(ctx) {
 export async function $userinfo__verify(ctx) {
   const response = await get__userinfo__auth0(ctx)
   if (!response.ok) {
+    error(`${logPrefix}|$userinfo__verify|!response.ok ${ctx.request.method} ${ctx.request.path}`)
+    error(`${response.status} ${response.message || ''}`)
+    if (response.status == 429) {
+      const remaining__ratelimit =
+              $remaining__ratelimit(response)
+      return $userinfo__waitfor__ratelimit(remaining__ratelimit)
+    }
     throw__bad_credentials(ctx)
   }
-  const userinfo = await response.json()
-  return userinfo
+  return await response.json()
+  async function $userinfo__waitfor__ratelimit(delay) {
+    let response
+      , done = false
+      , n__delay = 1
+    while (!done) {
+      await sleep(delay)
+      response = await get__userinfo__auth0(ctx)
+      if (response.ok) {
+        return response.json()
+      }
+      if (response.status === 429) {
+        const delay__ =
+                fibonacci(n__delay) * 500
+        delay = $remaining__ratelimit(response) + delay__
+        n__delay++
+        continue
+      }
+      done = true
+    }
+  }
+  function $remaining__ratelimit(response) {
+    return (
+      parseInt(
+        response.headers.get('x-ratelimit-remaining'))
+      || 1000
+    )
+  }
 }
