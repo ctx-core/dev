c7e12caba9b0e032e27bb20220c4a687eceeda09
Brian Takita <brian.takita@gmail.com>
2018-04-21 10:05:14 -0400

    uri/lib.mjs
      * tabs
      + _filename__url

--- uri/lib.mjs
+++ uri/lib.mjs
@@ -10,12 +10,17 @@ const logPrefix = 'ctx-core/uri/lib.mjs'
  * @see {@link https://danielmiessler.com/study/url-uri/}
  */
 export function $url__urn(uri) {
-  log(`${logPrefix}|$url__urn`)
-  if (!uri) return uri
-  if (
-    uri.indexOf('//') === 0
-    || uri.indexOf('http://') === 0
-    || uri.indexOf('https://') === 0
-  ) return uri
-  return `//${uri}`
+	log(`${logPrefix}|$url__urn`)
+	if (!uri) return uri
+	if (
+		uri.indexOf('//') === 0
+		|| uri.indexOf('http://') === 0
+		|| uri.indexOf('https://') === 0
+	) return uri
+	return `//${uri}`
+}
+export function _filename__url(url) {
+	if (!url) return url
+	const location = url.split('?')[0]
+	return location.split('/').pop()
 }
