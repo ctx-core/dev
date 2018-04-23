52419cc641c0b6adf626288ffde4c847c3ae3c58
Brian Takita <brian.takita@gmail.com>
2018-04-22 22:35:17 -0400

    html/lib.mjs
      + _attrs
      + _class
      + _style
      + _styles__obj
      + _html
      + _html__links
      + _html__js
      + _css__path__versioned
      + _versioned__js
      + _versioned
      + __versioned
      + _query__version

--- html/lib.mjs
+++ html/lib.mjs
@@ -10,7 +10,7 @@ const logPrefix = 'ctx-core/html/lib.mjs'
  * @param {Object} obj - Key/Value pairs of the attrs
  * @returns {String} The attrs for an html element
  */
-export function $attrs(obj) {
+export function _attrs(obj) {
   if (!obj) return ''
   let attrs = []
   for (let key in obj) {
@@ -18,6 +18,7 @@ export function $attrs(obj) {
   }
   return attrs.join(' ')
 }
+export const $attrs = _attrs
 /**
  * Returns class html attribute from obj
  * @param {Object} obj - key/value pairs of classes. Truthy values will have key class added. Falsy values will have key class ignored.
@@ -25,13 +26,14 @@ export function $attrs(obj) {
  * @example
  * $class({class_1: true, class_2: false, class_3: true}) // returns 'class_1 class_3'
  */
-export function $class(obj, ...array__classes) {
+export function _class(obj, ...array__classes) {
   const ar = [...array__classes]
   for (let key in obj) {
     if (obj[key]) ar.push(key)
   }
   return ar.join(' ')
 }
+export const $class = _class
 /**
  * Assigns additional styles to the style attribute on the HTMLElement el.
  * @param {module:ctx-core/dom/lib~HTMLElement} el - Element to set style on. Existing styles are kept unless overwritten by obj.
@@ -54,7 +56,7 @@ export function assign__style(el, styles) {
  * @example
  * $style({position: 'absolute, left: '5px'}) // returns 'position: absolute; left: 5px;'
  */
-export function $style(obj) {
+export function _style(obj) {
   const ar = []
   for (let key in obj) {
     const value = obj[key]
@@ -62,6 +64,7 @@ export function $style(obj) {
   }
   return ar.join(' ')
 }
+export const $style = _style
 /**
  * Parses a style string & returns an object with each style
  * @param {string} styles__strings
@@ -69,7 +72,7 @@ export function $style(obj) {
  * @example
  * $styles__obj('position: absolute; left: 5px;') // returns {position: 'absolute, left: '5px'}
  */
-export function $styles__obj(styles__strings) {
+export function _styles__obj(styles__strings) {
   const style__strings = (styles__strings || '').split(/ *; */)
       , styles = {}
   for (let i=0; i < style__strings.length; i++) {
@@ -81,12 +84,13 @@ export function $styles__obj(styles__strings) {
   }
   return styles
 }
+export const $styles__obj = _styles__obj
 /**
  * Returns a string of escaped html
  * @param {string} unsafe
  * @returns {XML|string} - Escaped HTML
  */
-export function $html(unsafe) {
+export function _html(unsafe) {
   return unsafe
          .replace(/&/g, '&amp;')
          .replace(/</g, '&lt;')
@@ -94,11 +98,12 @@ export function $html(unsafe) {
          .replace(/"/g, '&quot;')
          .replace(/'/g, '&#039;')
 }
+export const $html = _html
 /**
  * html for css link tags
  * @returns {string}
  */
-export function $html__links() {
+export function _html__links() {
   log(`${logPrefix}|$html__links`)
   const ctx = assign({
             css: [],
@@ -120,11 +125,12 @@ export function $html__links() {
   }
   return array__html__links.join('\n')
 }
+export const $html__links = _html__links
 /**
  * html for js script tags
  * @returns {string}
  */
-export function $html__js() {
+export function _html__js() {
   log(`${logPrefix}|$html__js`)
   const ctx =
           assign(
@@ -143,15 +149,17 @@ export function $html__js() {
   }
   return array__html__js.join('\n')
 }
+export const $html__js = _html__js
 /**
  * versioned css file url
  * @param src__script
  */
-export function $css__path__versioned(src__script) {
+export function _css__path__versioned(src__script) {
   log(`${logPrefix}|$js$path__versioned`)
   const extName = '.css'
-  return $versioned(`${src__script}${extName}`)
+  return _versioned(`${src__script}${extName}`)
 }
+export const $css__path__versioned = _css__path__versioned
 /**
  * @deprecated
  * @type {$html__js}
@@ -164,35 +172,39 @@ export const html_js = $html__js
  * @param opts
  * @returns {string}
  */
-export function $versioned__js(ctx, src__script, opts={}) {
+export function _versioned__js(ctx, src__script, opts={}) {
   const extName = (!opts.debug && ctx.minify) ? '.min.js' : '.js'
-  return $versioned(ctx, `${src__script}${extName}`)
+  return _versioned(ctx, `${src__script}${extName}`)
 }
+export const $versioned__js = _versioned__js
 /**
  * versioned file
  * @param {module:ctx-core/object/lib~ctx}
  * @param {string} url
  * @returns {string}
  */
-export function $versioned(ctx, url) {
+export function _versioned(ctx, url) {
   log(`${logPrefix}|versioned`)
-  return `${url}?${$query__version(ctx)}`
+  return `${url}?${_query__version(ctx)}`
 }
+export const $versioned = _versioned
 /**
  * $versioned with ctx
  * @param {module:ctx-core/object/lib~ctx}
  * @returns {string}
  */
-export function $$versioned(ctx) {
+export function __versioned(ctx) {
   log(`${logPrefix}|$$versioned`)
   return function $versioned__$$versioned() {
-    return $versioned(ctx, ...arguments)
+    return _versioned(ctx, ...arguments)
   }
 }
+export const $$versioned = __versioned
 /**
  * version query param
  * @returns {string}
  */
-export function $query__version(ctx) {
+export function _query__version(ctx) {
   return `v=${encodeURIComponent($version(ctx))}`
 }
+export const $query__version = _query__version
