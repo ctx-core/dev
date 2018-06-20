0895959141941f90a3a5a26fbe9366e6982c629d
Brian Takita <brian.takita@gmail.com>
2018-06-20 01:01:01 -0400

    store/lib.mjs
      * _store
        + observeDeep
        + observeMany
        + setDeep
        - set__clone
        - transaction

--- store/lib.mjs
+++ store/lib.mjs
@@ -1,40 +1,17 @@
 // TODO: Use when https://github.com/sveltejs/svelte-extras/issues/20 is resolved
 //import {observe} from 'svelte-extras'
-import {observe} from 'svelte-extras/dist/svelte-extras.es.js'
-import {assign, mixin, clone} from 'ctx-core/object/lib.mjs'
-import __Store from 'svelte/store.umd.js'
-const {Store} = __Store
+import {setDeep, observe, observeDeep, observeMany} from 'svelte-extras/dist/svelte-extras.es.js'
+import {mixin, clone} from 'ctx-core/object/lib.mjs'
+import {Store} from 'svelte/store'
 export function _store() {
 	const store = new Store(...arguments)
 	mixin(store, {
+		observe,
+		observeDeep,
+		observeMany,
+		setDeep,
 		clone__get() {
 			return clone(this.get(), ...arguments)
-		},
-    set__clone(__set) {
-      const state = store.get()
-      const __ = {}
-      for (let key in __set) {
-        __[key] = clone(state[key], __set[key])
-      }
-      return store.set(__)
-    },
-		/**
-		 * TODO: Revisit when https://github.com/sveltejs/svelte/issues/1535 is resolved
-		 * @see {@link https://github.com/sveltejs/svelte/issues/1535}
-		 */
-		async transaction(fn) {
-			const store__transaction = mixin(new Store(), this)
-			store__transaction._state = clone(this._state)
-			store__transaction._computed = clone(this._computed)
-			const __set = {}
-			store__transaction.set = __set__ => {
-				assign(__set, __set__)
-				assign(store__transaction._state, __set__)
-			}
-			store__transaction._state = clone(store__transaction._state)
-			await Promise.all([fn(store__transaction)])
-			this.set(__set)
-			return store__transaction
 		},
 		get store() {return this.get().store},
 	})
