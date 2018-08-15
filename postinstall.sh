c5cdf5f67fc869de7b38708950575535e76d0d7f
Brian Takita <brian.takita@gmail.com>
2018-08-15 00:51:13 -0400

    postinstall.sh
    	fix(@ctx-core): using cd instead of pushd/popd for broader compatability

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,7 @@ echo 'postinstall.sh'
 echo "Environment: $NODE_ENV"
 
 DIR="$(pwd)"
-pushd node_modules/.bin
+cd node_modules/.bin
 DIR__BIN="$(pwd)"
 for f in $(find $DIR/*/bin/*); do
 	ln -sf "$f"
@@ -11,7 +11,7 @@ done
 for f in $(find $DIR/bin/*); do
 	ln -sf "$f"
 done
-popd
+cd $DIR
 
 echo $POSTINSTALL
 [ -z "$POSTINSTALL" ] || "$POSTINSTALL"
