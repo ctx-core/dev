46560abc9eb219fc0a94bcfd87015958d0ab601a
Brian Takita <brian.takita@gmail.com>
2018-09-03 00:52:40 -0400

    - packages/local-storage/agent.js
    - packages/local-storage/agent.mjs
    
    	BREAKING CHANGE: moved _ctx__set__json__from__localStorage
    	BREAKING CHANGE: moved sync__json__localStorage
    	BREAKING CHANGE: moved store__json__localStorage
    	BREAKING CHANGE: moved store__localStorage
    
    packages/local-storage/lib.mjs
    
    	+ _ctx__set__json__from__localStorage
    	+ sync__json__localStorage
    	+ store__json__localStorage
    	+ store__localStorage

--- packages/local-storage/lib.mjs
+++ packages/local-storage/lib.mjs
@@ -1,5 +1,5 @@
 import { assign } from '@ctx-core/object/lib.mjs'
-import { log, debug } from '@ctx-core/logger/lib.mjs'
+import { log, warn, debug } from '@ctx-core/logger/lib.mjs'
 const logPrefix = '@ctx-core/local-storage/lib.mjs'
 export function load__ctx__localStorage() {
 	log(`${logPrefix}|load__ctx__localStorage`)
@@ -30,3 +30,37 @@ export function remove__ctx__localStorage(...args) {
 	set__ctx__localStorage(ctx)
 	return ctx
 }
+export function _ctx__set__json__from__localStorage(scope__) {
+	log(`${logPrefix}|_ctx__set__json__from__localStorage`)
+	const json = localStorage.getItem(scope__)
+	const ctx__set = {}
+	if (json) {
+		try {
+			ctx__set[scope__] = JSON.parse(json)
+		} catch (e) {
+			warn(`${logPrefix}|_ctx__set__json__from__localStorage|error|JSON.parse`)
+			warn(e)
+		}
+	}
+	return ctx__set
+}
+export function sync__json__localStorage(key, value) {
+	log(`${logPrefix}|sync__json__localStorage`, key)
+	if (value == null) {
+		localStorage.removeItem(key)
+	} else {
+		localStorage.setItem(key, JSON.stringify(value))
+	}
+	return value
+}
+export const store__json__localStorage = sync__json__localStorage
+export function sync__localStorage(key, value) {
+	log(`${logPrefix}|sync__localStorage`, key)
+	if (value == null) {
+		localStorage.removeItem(key)
+	} else {
+		localStorage.setItem(key, value)
+	}
+	return value
+}
+export const store__localStorage = sync__localStorage
