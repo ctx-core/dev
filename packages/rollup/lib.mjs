23d5b180bcbedf00e2d78efaecb89a44dd9db374
Brian Takita <brian.takita@gmail.com>
2018-09-05 22:27:38 -0400

    packages/carousel/package.json
    	- svelte-awesome
    
    packages/rollup/lib.mjs
    
    	- svelte-awesome

--- packages/rollup/lib.mjs
+++ packages/rollup/lib.mjs
@@ -69,8 +69,7 @@ export function _external__node__rollup(opts = {}) {
 	const exclude__concat__exclude__opts = [
 		'@ctx-core',
 		'svelte',
-		'svelte-extras',
-		'svelte-awesome',
+		'svelte-extras'
 	].concat(exclude__opts)
 	const externals =
 		_externals__node_modules({
