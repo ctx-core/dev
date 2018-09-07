e8890d974f8b2ad15173de7ef49b3ecb4abdea11
Brian Takita <brian.takita@gmail.com>
2018-09-07 17:27:36 -0400

    packages/sass/bin/sass-build.sh
    	echo output of sass-cmd.js

--- packages/sass/bin/sass-build.sh
+++ packages/sass/bin/sass-build.sh
@@ -2,5 +2,8 @@
 # SEE https://github.com/sass/node-sass/issues/1579
 # TODO: Use yarn when https://github.com/yarnpkg/yarn/issues/2069 is resolved
 npm rebuild node-sass
+CMD__SASS="$(sass-cmd.js $@ -- '&')"
+echo "$CMD__SASS"
+eval "$CMD__SASS"
 eval "$(sass-cmd.js $@ -- '&')"
 wait
