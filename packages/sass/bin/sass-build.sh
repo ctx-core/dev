d62d3671d494e81bd38bb469d8a40b31eaa6b848
Brian Takita <brian.takita@gmail.com>
2018-09-07 17:41:04 -0400

    packages/sass/bin/sass-build.sh
    	+ echo sass-build.sh

--- packages/sass/bin/sass-build.sh
+++ packages/sass/bin/sass-build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo sass-build.sh
 # SEE https://github.com/sass/node-sass/issues/1579
 # TODO: Use yarn when https://github.com/yarnpkg/yarn/issues/2069 is resolved
 npm rebuild node-sass
