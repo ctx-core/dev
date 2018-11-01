d12218897e466ed1fa704f0efbd5c952302069f8
Brian Takita <brian.takita@gmail.com>
2018-11-01 03:10:15 -0400

    packages/tree__checkbox/Node__Tree__Checkbox.html
    	+ ARR__key__child
    		- keys__children
    		BREAKING CHANGE: @ctx-core/tree__checkbox: ARR__key__child instead of keys__children
    
    + packages/tree__checkbox/dom.mjs
    
    	+ _fn__change__checkbox__all
    		feat(@ctx-core/tree__checkbox): _fn__change__checkbox__all
    	+ _fn__change__checkbox
    		feat(@ctx-core/tree__checkbox): _fn__change__checkbox

---
+++ packages/tree__checkbox/dom.mjs
@@ -0,0 +1,31 @@
+import { clone } from '@ctx-core/object/lib.mjs'
+import { log, debug } from '@ctx-core/logger/lib.mjs'
+const logPrefix = '@ctx-core/tree__checkbox/dom.mjs'
+export function _fn__change__checkbox__all(key__store, fn) {
+	return function __change__checkbox__all(event) {
+		log(`${logPrefix}|__change__checkbox__all`, event)
+		const { store } = this
+		const { currentTarget } = event
+		const { checked } = currentTarget
+		const selected__BY__item = clone(store.get()[key__store])
+		fn.call(this, selected__BY__item, checked)
+		store.set({
+			[key__store]: selected__BY__item,
+		})
+	}
+}
+export function _fn__change__checkbox(key__store) {
+	return function __change__checkbox(event, item) {
+		log(`${logPrefix}|__change__checkbox`, { event, item })
+		const { store } = this
+		const { currentTarget } = event
+		const { checked } = currentTarget
+		const selected__BY__item = clone(store.get()[key__store])
+		if (!!(checked) != !!(selected__BY__item[item])) {
+			selected__BY__item[item] = checked
+			store.set({
+				[key__store]: selected__BY__item,
+			})
+		}
+	}
+}
