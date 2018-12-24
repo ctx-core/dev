9e2aa1765a5ec1c983f7cd2f71181038cd99defd
Brian Takita <brian.takita@gmail.com>
2018-12-23 19:01:12 -0500

    packages/carousel/Carousel.html
    	Icon[name=chevron-left-solid]
    	Icon[name=chevron-right-solid]
    	∋ register__chevron_left_solid
    	∋ register__chevron_right_solid
    
    packages/font-awesome/lib.mjs
    
    	feat(@ctx-core/font-awesome):
    	+ register__chevron_up_solid
    		feat(@ctx-core/font-awesome): register__chevron_up_solid
    	+ register__chevron_down_solid
    		BREAKING CHANGE: @ctx-core/font-awesome: register__chevron_down_solid
    		- register__chevron_down
    	+ register__chevron_left_solid
    		BREAKING CHANGE: @ctx-core/font-awesome: register__chevron_left_solid
    		- register__chevron_left
    	+ register__chevron_right_solid
    		BREAKING CHANGE: @ctx-core/font-awesome: register__chevron_right_solid
    		- register__chevron_right

--- packages/font-awesome/lib.mjs
+++ packages/font-awesome/lib.mjs
@@ -1,12 +1,16 @@
-export function register__chevron_down(Icon) {
-	if (Icon.icons['chevron-down']) return
-	Icon.register({ 'chevron-down': { width: 1792, height: 1792, paths: [{ d: 'M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z' }] } })
+export function register__chevron_up_solid(Icon) {
+	if (Icon.icons['chevron-up-solid']) return
+	Icon.register({ 'chevron-up-solid': { width: 448, height: 512, paths: [{ d: 'M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z' }] } })
 }
-export function register__chevron_left(Icon) {
-	if (Icon.icons['chevron-left']) return
-	Icon.register({ 'chevron-left': { width: 1792, height: 1792, paths: [{ d: 'M1171 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z' }] } })
+export function register__chevron_down_solid(Icon) {
+	if (Icon.icons['chevron-down-solid']) return
+	Icon.register({ 'chevron-down-solid': { width: 448, height: 512, paths: [{ d: 'M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z' }] } })
 }
-export function register__chevron_right(Icon) {
-	if (Icon.icons['chevron-right']) return
-	Icon.register({ 'chevron-right': { width: 1280, height: 1792, paths: [{ d: 'M1107 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z' }] } })
+export function register__chevron_left_solid(Icon) {
+	if (Icon.icons['chevron-left-solid']) return
+	Icon.register({ 'chevron-left-solid': { width: 320, height: 512, paths: [{ d: 'M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z' }] } })
+}
+export function register__chevron_right_solid(Icon) {
+	if (Icon.icons['chevron-right-solid']) return
+	Icon.register({ 'chevron-right-solid': { width: 1280, height: 1792, paths: [{ d: 'M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z' }] } })
 }
