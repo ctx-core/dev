d63a37d66169228a401c5fa3bd5c405207728cf9
Brian Takita <brian.takita@gmail.com>
2019-03-04 02:27:31 -0500

    packages/route/store.js
    	BREAKING CHANGE: svelte-3.0.0-beta.9
    
    packages/store/lib.js
    
    	+ symbol__loaded
    	_load__store
    		return async (force = false)
    		feat(@ctx-core/store): _load__store can be forced
    		feat(@ctx-core/store): load__ARR__store
    		feat(@ctx-core/store): reload__ARR__store

--- packages/route/store.js
+++ packages/route/store.js
@@ -1,57 +1,27 @@
-import { _mixin__store, compute } from '@ctx-core/store/lib.js'
-import { mixin, clone, _ctx__zip } from '@ctx-core/object/lib.js'
+import { writable, derive } from 'svelte/store.mjs'
+import { writable__load } from '@ctx-core/store/lib.js'
 import { _router } from './lib.js'
 import { log, debug } from '@ctx-core/logger/lib.js'
 const logPrefix = '@ctx-core/route/store.js'
-export const __store__route = _mixin__store('__store__route', async store => {
-	const scope = [
-		'route',
-		'query__route'
-	]
-	mixin(store, {
-		set__route(__set) {
-			this.set(
-				clone(
-					{
-						route: '',
-						query__route: {}
-					},
-					__set))
-			return this
-		},
-	})
-	compute(store, {
-		__route: [
-			scope,
-			(...values) => _ctx__zip(scope, values)
-		]
-	})
-})
-export const __store__router = _mixin__store('__store__router', async store => {
-	const scope = ['router']
-	mixin(store, {
-		reset__router() {
-			this.set({ router: _router() })
-			return this
-		},
-		add__route() {
-			this.ensure__router().add(...arguments)
-			return this
-		},
-		listen__router() {
-			this.ensure__router().listen()
-			return this
-		},
-		navigate() {
-			this.ensure__router().navigate(...arguments)
-			this.fire('navigate')
-			return this
-		},
-		ensure__router() {
-			if (!this.router) this.reset__router()
-			return this.router
-		},
-		get router() {return this.get().router}
-	})
-	return store.reset__router()
-})
+export const route = writable('')
+export const query__route = writable({})
+export const __route = derive([route, query__route],
+	($route, $query__route) => ({
+		route: $route,
+		query__route: $query__route,
+	}))
+export const router = writable__load(writable(), [], _router)
+export async function ensure__router() {
+	let value
+	router.update($router => value = ($router || _router()))
+	return value
+}
+export function add__route() {
+	ensure__router().add(...arguments)
+}
+export function listen__router() {
+  ensure__router().listen()
+}
+export function navigate() {
+  ensure__router().navigate(...arguments)
+}
--- packages/store/lib.js
+++ packages/store/lib.js
@@ -2,18 +2,15 @@ import { derive } from 'svelte/store.mjs'
 import { get_store_value } from 'svelte/internal.mjs'
 import { map } from '@ctx-core/array/lib.js'
 const symbol__load = Symbol('load')
+const symbol__loaded = Symbol('loaded')
 export const get = get_store_value
 export { get_store_value }
-export async function load__ARR__store(ARR__store = []) {
-	const ARR__load__store = map(ARR__store, store => store[symbol__load])
-	return await Promise.all(ARR__load__store)
-}
 export function _load__store(store, deps, fn) {
-	return async () => {
-		if (store.__loaded) return store
+	return async (force = false) => {
+		if (force || store[symbol__loaded]) return store
 		const deps__ = map(deps, dep => dep[symbol__load] || dep)
 		await Promise.all(deps__)
-		store.__loaded = true
+		store[symbol__loaded] = true
 		if (fn) await fn(store)
 		return store
 	}
@@ -25,10 +22,20 @@ export function _load__writable(store, deps, fn__writable) {
 }
 export function derive__load(deps, fn) {
 	const store = derive(deps, fn)
-  store[symbol__load] = _load__store(store, deps, fn)
+	store[symbol__load] = _load__store(store, deps, fn)
 	return store
 }
 export function writable__load(store, deps, fn__writable) {
-  store[symbol__load] = _load__writable(store, deps, fn__writable)
+	store[symbol__load] = _load__writable(store, deps, fn__writable)
 	return store
+}
+export async function load__ARR__store(ARR__store = []) {
+	const ARR__load__store = map(ARR__store, store => store[symbol__load])
+	return await Promise.all(ARR__load__store)
+}
+export async function reload__ARR__store(ARR__store = []) {
+	const ARR__promise = map(
+		ARR__store,
+		store => store[symbol__load] && store[symbol__load](true))
+	return await Promise.all(ARR__promise)
 }
