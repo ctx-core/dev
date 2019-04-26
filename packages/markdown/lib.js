e0bfb569b5fbe09a44d7bdc7003e0eb3b036b187
Brian Takita <brian.takita@gmail.com>
2019-04-26 05:45:46 -0400

    + packages/markdown/lib.js
    	+ _obj__metadata__content
    	+ _html__content__markdown
    
    packages/markdown/package.json
    
    	∋ "highlight.js": "^9.15.6"
    	∋ "marked": "^0.6.2"

---
+++ packages/markdown/lib.js
@@ -0,0 +1,28 @@
+// See https://github.com/sveltejs/sapper.svelte.technology/blob/master/src/routes/guide/_process_markdown.js
+import hljs from 'highlight.js/lib/highlight'
+import marked from 'marked'
+export function _obj__metadata__content(markdown) {
+	const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(markdown)
+	const frontMatter = match[1]
+	const content = markdown.slice(match[0].length)
+	const metadata = {}
+	frontMatter.split('\n').forEach(pair => {
+		const colonIndex = pair.indexOf(':')
+		metadata[pair.slice(0, colonIndex).trim()] = pair
+			.slice(colonIndex + 1)
+			.trim()
+	})
+	return { metadata, content }
+}
+export const _obj__metadata__content__markdown = _obj__metadata__content
+export function _html__content__markdown(content) {
+	const renderer = new marked.Renderer()
+	renderer.code = (source, lang) => {
+		const highlighted = hljs.highlight(lang, source).value
+		return `<pre><code>${highlighted}</code></pre>`
+	}
+	const html = marked(content.replace(/^\t+/gm, match => match.split('\t').join('  ')), {
+		renderer
+	})
+	return html
+}
