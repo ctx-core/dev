3e553fea24d07e4347d787144c6da5c29830d615
Brian Takita <brian.takita@gmail.com>
2019-04-29 16:44:33 -0400

    packages/store/lib.js
    	feat(@ctx-core/store): storable

--- packages/store/lib.js
+++ packages/store/lib.js
@@ -156,3 +156,27 @@ export function clear__a1__store(a1__store = [], value = null) {
 export function _clear__a1__store(a1__store = [], value = null) {
 	return () => clear__a1__store(a1__store, value)
 }
+const storage = typeof localStorage !== 'undefined' ? localStorage : {
+	removeItem: () => {},
+}
+/**
+ * Tracks storage both in `localStorage` and in svelte's `writable` stores
+ * Usage: `const name = storable('name', 'jimmy')`
+ * @param {string} key        - `localStorage` key
+ * @param {any} value        - default/initial value (if value is already set in `localStorage`, it will load that value instead)
+ * @param {Function} fn        - handed off to `writable`
+ */
+export const storable = (key, value, fn) => {
+	key = `cm.store.${key}`
+	if (storage[key]) { value = JSON.parse(storage[key]) }
+	const store = writable(value, fn)
+	store.subscribe(value => {
+		if (value === undefined) {
+			storage.removeItem(key)
+		} else {
+			storage[key] = JSON.stringify(value)
+		}
+	})
+	store.remove = () => store.set(undefined)
+	return store
+}
