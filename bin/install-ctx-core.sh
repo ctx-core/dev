c8d87e57474a3d9647ebd34c923618d4ce50ee02
Brian Takita <brian.takita@gmail.com>
2019-04-29 20:24:52 -0400

    - GraphViz/GraphViz.md
    bin/install-ctx-core.sh
    
    	fix(@ctx-core/dev): install-ctx-core.sh: incorrect reference to .git/modules/packages/ctx-core
    
    git__ctx-core/config
    
    	fix(@ctx-core/dev): incorrect worktree

--- bin/install-ctx-core.sh
+++ bin/install-ctx-core.sh
@@ -13,10 +13,10 @@ while getopts ":l:h:" o; do
 done
 shift $((OPTIND-1))
 if [ -z $(git submodule | awk '{print $2}' | grep ctx-core) ]; then
-	git submodule add https://github.com/ctx-core/ctx-core.git ctx-core
+	git submodule add https://github.com/ctx-core/ctx-core.git packages/ctx-core
 fi
 if [ -z "${l}" ]; then
-	cp -f ./ctx-core/git__ctx-core/config .git/modules/ctx-core/config
+	cp -f ./packages/ctx-core/git__ctx-core/config .git/modules/packages/ctx-core/config
 else
-	wget https://raw.githubusercontent.com/ctx-core/ctx-core/git__ctx-core/config --output-file .git/modules/ctx-core/config
+	wget https://raw.githubusercontent.com/ctx-core/ctx-core/git__ctx-core/config --output-file .git/modules/packages/ctx-core/config
 fi
