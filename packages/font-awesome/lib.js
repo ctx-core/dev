f3fa87d80d6043ee02dd03ee85f51be1a9a7d5f8
Brian Takita <brian.takita@gmail.com>
2019-05-01 09:30:01 -0400

    packages/font-awesome/Icon.html
    	feat(@ctx-core/font-awesome): name attribute is an unwrapped 1d array, enabling trying icons which may not be registered
    	∋ or
    	∋ _a1__wrap
    
    packages/font-awesome/lib.js
    
    	feat(@ctx-core/font-awesome): register__arrow_right_solid
    
    packages/font-awesome/package.json
    
    	↑ "@ctx-core/array": "^12.0.1"
    
    packages/nav/lib.scss
    
    	Handle__Nav
    		$width__wide instead of $width__widescreen

--- packages/font-awesome/lib.js
+++ packages/font-awesome/lib.js
@@ -35,6 +35,10 @@ export function register__arrow_down_solid() {
 	if (_has__icon('arrow-down-solid')) return
 	register__icon({ 'arrow-down-solid': { width: 448, height: 512, paths: [{ d: 'M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z' }] } })
 }
+export function register__arrow_right_solid() {
+	if (_has__icon('arrow-right-solid')) return
+	register__icon({ 'arrow-right-solid': { width: 448, height: 512, paths: [{ d: 'M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z' }] } })
+}
 export function register__bars_solid() {
 	if (_has__icon('bars-solid')) return
 	register__icon({ 'bars-solid': { width: 448, height: 512, paths: [{ d: 'M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z' }] } })
