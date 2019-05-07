8f379c2ee2c445476ecbf6199fa7fc146236588e
Brian Takita <brian.takita@gmail.com>
2019-05-07 16:15:13 -0400

    packages/env/store.js
    	fix(@ctx-core/env): error when process is undefined
    
    packages/etsy_cache/package.json
    
    	↑ "aws-sdk": "^2.451.0"
    
    packages/instagram_cache/package.json
    
    	↑ "aws-sdk": "^2.451.0"

--- packages/env/store.js
+++ packages/env/store.js
@@ -1,7 +1,11 @@
 import { writable, derived } from 'svelte/store'
 export const __NODE_ENV = writable(process.env.NODE_ENV)
-export const __CACHE_VERSION = writable(process.env.CACHE_VERSION)
-export const __VERSION = writable(process.env.HEROKU_SLUG_COMMIT || Math.random())
+export const __CACHE_VERSION =
+	writable(typeof process === 'object' && process.env.CACHE_VERSION)
+export const __VERSION =
+	writable(
+		(typeof process === 'object' && process.env.HEROKU_SLUG_COMMIT)
+		|| Math.random())
 export const __is__production =
 	derived(__NODE_ENV, NODE_ENV => NODE_ENV === 'production')
 export const __is__staging =
