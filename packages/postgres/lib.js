3be8b32ae09fb645b1d614e672ded2c09325c64a
Brian Takita <brian.takita@gmail.com>
2019-05-11 19:57:16 -0400

    packages/postgres/lib.js
    	+ _a1__named_param
    	+ _sql__set__a1__param

--- packages/postgres/lib.js
+++ packages/postgres/lib.js
@@ -1,34 +1,36 @@
 import { map } from '@ctx-core/array'
-export function _ARR__named_param(ARR__param) {
+export function _a1__named_param(a1__param) {
 	return (
 		map(
-			ARR__param,
+			a1__param,
 			(_, i) => `$${i+1}`
 		)
 	)
 }
-export const _ARR__named_param__pg = _ARR__named_param
+export const _ARR__named_param = _a1__named_param
+export const _ARR__named_param__pg = _a1__named_param
 /**
- * Set Add named param. Returns index of value, which is already in ARR__param or pushed onto ARR__param.
- * @param ARR__param
+ * Set Add named param. Returns index of value, which is already in a1__param or pushed onto a1__param.
+ * @param a1__param
  * @param value
  * @returns {*}
  */
-export function sadd__named_param(ARR__param, value) {
-	const indexOf__ = ARR__param.indexOf(value)
+export function sadd__named_param(a1__param, value) {
+	const indexOf__ = a1__param.indexOf(value)
 	if (indexOf__ > -1) {
 		return indexOf__
 	} else {
-		ARR__param.push(value)
-		const index__append = ARR__param.length
+		a1__param.push(value)
+		const index__append = a1__param.length
 		return index__append
 	}
 }
-export function _sql__set__ARR__param(ARR__param, ARR__named_param) {
+export function _sql__set__a1__param(a1__param, a1__named_param) {
   return (
   	map(
-  		ARR__param,
-			(param, i) => `${param}=${ARR__named_param[i]}`
+  		a1__param,
+			(param, i) => `${param}=${a1__named_param[i]}`
 		).join(', ')
 	)
 }
+export const _sql__set__ARR__param = _sql__set__a1__param
