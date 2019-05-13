5808268c9492b8d916ba8893b71d34251a96bdae
Brian Takita <brian.takita@gmail.com>
2019-05-12 22:23:12 -0400

    packages/font-awesome/Icon.html
    	- icons
    
    packages/markdown/lib.js
    
    	∋ fs.lstat
    	feat(@ctx-core/markdown): _get__a1__segment
    
    packages/nav/Item__Nav.html
    
    	feat(@ctx-core/nav): Item__Nav: on:click

--- packages/markdown/lib.js
+++ packages/markdown/lib.js
@@ -1,12 +1,13 @@
 // See https://github.com/sveltejs/sapper.svelte.technology/blob/master/src/routes/guide/_process_markdown.js
 import fs from 'fs'
-import { join, extname, basename } from 'path'
+import { join, extname, basename, resolve } from 'path'
 import { promisify } from 'util'
 import { map, filter } from '@ctx-core/array'
 import { _andand } from '@ctx-core/function'
 const exists = promisify(fs.exists)
-const readFile__promise = promisify(fs.readFile)
-const promise__readdir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+const readdir = promisify(fs.readdir)
+const lstat = promisify(fs.lstat)
 import marked from 'marked'
 export function _obj__metadata__content(markdown) {
 	const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(markdown)
@@ -39,11 +40,13 @@ export function _html__markdown(markdown, opts = {}) {
 	})
 	return html
 }
-export async function _content__md__file(relative_path, params = {}) {
-	if (extname(relative_path) !== '.md') return
-	const txt__path = join(process.cwd(), relative_path)
-	if (!(await exists(txt__path))) return
-	const markdown = await readFile__promise(txt__path, 'utf-8')
+export async function _content__md__file(txt__path, params = {}) {
+	if (extname(txt__path) !== '.md') return
+	const txt__path__resolve = resolve(txt__path)
+	if (!(await exists(txt__path__resolve))) return
+	const stats = await lstat(txt__path__resolve)
+	if (!stats.isFile()) return
+	const markdown = await readFile(txt__path__resolve, 'utf-8')
 	const { content, metadata } = _obj__metadata__content(markdown)
 	const html = _html__markdown(content, params)
 	return {
@@ -65,7 +68,10 @@ export async function _html__md__dir(dir) {
 }
 export async function _a1__name(params = {}) {
 	const { dir } = params
-	const a1__name__ext = await promise__readdir(dir)
+	if (!await exists(dir)) return
+	const stats = await lstat(dir)
+	if (!stats.isDirectory()) return
+	const a1__name__ext = await readdir(dir)
 	return (
 		map(
 			filter(a1__name__ext, name => extname(name) === '.md'),
@@ -94,9 +100,9 @@ export function _get__md__dir(params = {}) {
 		res.end(json)
 	}
 }
-export function _get__a1__name(params = {}) {
+export function _get__a1__name(opts = {}) {
 	return async (req, res) => {
-		const a1__name = await _a1__name(params)
+		const a1__name = await _a1__name(opts)
 		const json = JSON.stringify({ a1__name })
 		const headers = {
 			'Content-Type': 'application/json',
@@ -126,5 +132,59 @@ export function _get__md__file(opts = {}) {
 		}
 		res.writeHead(200, headers)
 		res.end(json)
+	}
+}
+export function _get__a1__segment(opts = {}) {
+	const { dir } = opts
+	return async (req, res) => {
+		console.debug('_get__a1__segment|debug|0')
+		const { params } = req
+		const { a1__segment } = params
+		console.debug('_get__a1__segment|debug|1', {
+			dir,
+			params,
+		})
+		const path__segment = a1__segment.join('/')
+		const path__content = join(dir, path__segment)
+		const path__content__resolve = resolve(path__content)
+		const dir__resolve = resolve(dir)
+		const valid__path =
+			path__content__resolve.slice(0, dir__resolve.length) === dir__resolve
+		if (!valid__path) {
+			res.writeHead(403)
+			res.end('Forbidden')
+			return
+		}
+		const headers = {
+			'Content-Type': 'application/json',
+		}
+		if (process.env.NODE_ENV !== 'development') {
+			headers['Cache-Control'] = `max-age=${5 * 60 * 1e3}` // 5 minutes
+		}
+		const a1__name = await _a1__name({ dir: path__content__resolve })
+		if (a1__name) {
+			res.writeHead(200, headers)
+			res.end(JSON.stringify({
+				a1__name,
+				path__content,
+				path__segment,
+			}))
+			return
+		}
+		const content__md = await _content__md__file(`${path__content__resolve}.md`, params)
+		if (content__md) {
+			res.writeHead(200, headers)
+			res.end(JSON.stringify({
+				content__md,
+				path__content: `${path__content}.md`,
+				path__segment: `${path__segment}.md`,
+			}))
+			return
+		}
+		res.writeHead(404, headers)
+		res.end(JSON.stringify({
+			status: 404,
+			message: 'Not Found',
+		}))
 	}
 }
