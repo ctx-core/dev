fdd129c2cf5f7d8e28f1da8c548f0b1f2405fd1a
Brian Takita <brian.takita@gmail.com>
2019-05-14 06:29:39 -0400

    packages/sass/package.json
    	∋ "@ctx-core/array": "^13.0.2"
    	∋ "css": "^2.2.4"
    
    packages/sass/svelte.js
    
    	feat(@ctx-core/sass): attributes.global: wrap selectors with a :global(.*) pseudo-selector

--- packages/sass/svelte.js
+++ packages/sass/svelte.js
@@ -1,5 +1,7 @@
 import sass from 'node-sass'
 import importer__package from 'node-sass-package-importer'
+import css from 'css'
+import { each, map } from '@ctx-core/array'
 export function style__sass({ content, attributes }) {
 	const { type } = attributes
 	if (type !== 'text/scss' && type !== 'text/sass') return
@@ -12,8 +14,20 @@ export function style__sass({ content, attributes }) {
 			outFile: 'x' // this is necessary, but is ignored
 		}, (err, result) => {
 			if (err) return reject(err)
+			let code = result.css.toString()
+			if (attributes.global) {
+				const ast = css.parse(code)
+				const { rules } = ast.stylesheet
+				each(rules, rule => {
+					const { selectors } = rule
+					rule.selectors = map(selectors, selector =>
+						`:global(${selector.replace(/:global\((.*)\)/g, '$1')})`
+					)
+				})
+				code = css.stringify(ast)
+			}
 			fulfil({
-				code: result.css.toString(),
+				code,
 				map: result.map.toString()
 			})
 		})
