d0d02376b4ae42136ad1517a3bc276fa3099dbec
Brian Takita <brian.takita@gmail.com>
2019-05-18 12:12:26 -0400

    packages/font-awesome/[name__icon].svg.js
    	fix(@ctx-core/font-awesome): resolve error
    
    packages/socicon/[name__icon].svg.js
    
    	fix(@ctx-core/socicon): resolve error

--- packages/font-awesome/[name__icon].svg.js
+++ packages/font-awesome/[name__icon].svg.js
@@ -1,6 +1,7 @@
 import { _get as _get__svg } from '@ctx-core/svg/[name__icon].svg'
 import { join } from 'path'
-const resolve = require('resolve')
+import { promisify } from 'util'
+const resolve = promisify(require('resolve'))
 export function _get(opts = {}) {
   const { fn } = opts
 	return _get__svg({
