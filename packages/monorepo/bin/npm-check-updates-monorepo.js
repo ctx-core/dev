9a7b26cfed5b518faec22239e481026ddbb4d159
Brian Takita <brian.takita@gmail.com>
2019-05-31 16:24:10 -0400

    ↑ "aws-sdk": "^2.466.0"
    	packages/etsy_cache/package.json
    	packages/instagram_cache/package.json
    
    ↑ "apollo-server-express": "^2.6.0"
    
    	packages/graphql/package.json
    
    ↑ "highlight.js": "^9.15.8"
    
    	packages/highlight.js/package.json
    	packages/markdown/package.json
    
    ↑ "rollup-pluginutils": "^2.8.0"
    
    	packages/nodent/package.json
    
    ↑ "papaparse": "^5.0.0"
    
    	packages/papaparse/package.json
    
    ↑ "node-sass-package-importer": "^5.3.2"
    
    	packages/sass/package.json
    
    packages/monorepo/bin/npm-check-updates-monorepo.js
    
    packages/monorepo/lib.js
    
    	fix(@ctx-core/monorepo): npm_check_updates__monorepo: packages were not being upgraded

--- packages/monorepo/bin/npm-check-updates-monorepo.js
+++ packages/monorepo/bin/npm-check-updates-monorepo.js
@@ -3,9 +3,9 @@ require = require('esm')(module)
 const { npm_check_updates__monorepo } = require('../lib.js')
 main()
 async function main() {
-	const stdout__BY__name__workspace = await npm_check_updates__monorepo()
-	for (let name__workspace in stdout__BY__name__workspace) {
+	const h1__name__workspace__h0__stdout = await npm_check_updates__monorepo()
+	for (let name__workspace in h1__name__workspace__h0__stdout) {
 		console.info(name__workspace)
-		console.info(stdout__BY__name__workspace[name__workspace])
+		console.info(h1__name__workspace__h0__stdout[name__workspace])
 	}
 }
