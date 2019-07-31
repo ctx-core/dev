5af78f0fddfc61ddd62a232f16876daa429ee88f
Brian Takita <brian.takita@gmail.com>
2019-07-31 11:25:06 -0400

    BREAKING CHANGE: @ctx-core/dialog: api changes
    	+ __stack__dialog
    	+ __top__stack__dialog
    	+ add__component__stack__dialog
    	+ remove__component__stack__dialog
    	+ close__top__stack__dialog
    	+ close__stack__dialog
    	- __dialogs
    	- __dialogs__prev
    	- __dialog
    	- push__dialogs
    	- remove__dialogs
    	- findBy__name__tag__dialogs
    
    @ctx-core/dialog
    
    	- @ctx-core/layer
    	- @ctx-core/logger
    	- @ctx-core/object
    	- @ctx-core/route
    
    BREAKING CHANGE: @ctx-core/layer: api changes
    
    	+ __a1__layer: - __layers
    	+ __top__layer: - __top__layers
    	+ __zIndex__top__layers: - __zIndex__top__layers
    	+ push__a1__layer: - push__layers
    	+ unshift__a1__layer: - unshift__layers
    	+ remove__a1__layer: - remove__layers

--- packages/dialog/store.js
+++ packages/dialog/store.js
@@ -1,56 +1,23 @@
-import { get, writable } from 'svelte/store'
-import { push__layers, remove__layers } from '@ctx-core/layer/store'
-import { compact, _last, _difference } from '@ctx-core/array'
-import { log } from '@ctx-core/logger'
-import { _has__dom } from '@ctx-core/dom'
-import { subscribe, _set__store } from '@ctx-core/store'
-export const __dialogs = writable(null)
-export const __dialogs__prev = writable(null)
-export const __dialog = writable(null)
-if (_has__dom()) {
-	subscribe(__dialogs,
-		_set__store(__dialog, dialogs => _last(dialogs)))
+import { writable, derived, get } from 'svelte/store'
+import { subscribe__debug } from '@ctx-core/store'
+import { each, _last, remove } from '@ctx-core/array'
+export const __stack__dialog = writable([])
+export const __top__stack__dialog = derived(__stack__dialog, _last)
+export function add__component__stack__dialog(component) {
+	const stack__dialog = get(__stack__dialog).slice(0)
+	stack__dialog.push(component)
+	__stack__dialog.set(stack__dialog)
 }
-export function clear__dialogs() {
-	__dialogs.set(null)
+export function remove__component__stack__dialog(component) {
+	const stack__dialog = get(__stack__dialog).slice(0)
+	remove(stack__dialog, component)
+	__stack__dialog.set(stack__dialog)
 }
-export function push__dialogs(...dialogs__) {
-	log(`${logPrefix}|push__dialogs`)
-	const layers = []
-	for (let i = 0; i < dialogs__.length; i++) {
-		const dialog = dialogs__[i]
-		dialog.layer = dialog.layer || {}
-		layers.push(dialog.layer)
-	}
-	push__layers(layers)
-	const dialogs = get(__dialogs).slice(0)
-	dialogs.push(...dialogs__)
-	__dialogs.set(dialogs)
+export function close__top__stack__dialog(event) {
+	const top__stack__dialog = get(__top__stack__dialog)
+	if (top__stack__dialog) top__stack__dialog.close(event)
 }
-export function remove__dialogs(...dialogs__) {
-	log(`${logPrefix}|remove__dialogs`)
-	const dialogs__remove__ = compact(dialogs__ || [])
-	const dialogs__remove = []
-	const layers__remove = []
-	for (let i = 0; i < dialogs__remove__.length; i++) {
-		const dialog__remove__ = dialogs__remove__[i]
-		const dialog__remove =
-			typeof dialog__remove__ === 'string'
-			? findBy__name__tag__dialogs(dialog__remove__)
-			: dialog__remove__
-		dialogs__remove.push(dialog__remove)
-		layers__remove.push(dialog__remove.layer)
-	}
-	remove__layers(...layers__remove)
-	__dialogs.set(_difference(dialogs__remove, get(__dialogs)))
-}
-export function findBy__name__tag__dialogs(name__tag) {
-	log(`${logPrefix}|findBy__name__tag__dialogs`, name__tag)
-	const dialogs = get(__dialogs)
-	for (let i = 0; i < dialogs.length; i++) {
-		const dialog = dialogs[i]
-		if (dialog.name__tag === name__tag) {
-			return dialog
-		}
-	}
+export function close__stack__dialog(event) {
+	const stack__dialog = get(__stack__dialog)
+	each(stack__dialog, dialog => dialog.close(event))
 }
