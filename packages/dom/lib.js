a13d4f217b1e237492c87af659994ef73ef26d6f
Brian Takita <brian.takita@gmail.com>
2019-08-03 00:34:04 -0400

    feat: @ctx-core/dom: trigger__native_event
    feat: @ctx-core/dom: trigger__custom_event

--- packages/dom/lib.js
+++ packages/dom/lib.js
@@ -442,3 +442,20 @@ export function _bcr__offset(node) {
 		y: offsetTop,
 	}
 }
+export function trigger__native_event(node, event_name, bubbles = true, cancelable = false) {
+	const event = document.createEvent('HTMLEvents')
+	event.initEvent(event_name, bubbles, cancelable)
+	node.dispatchEvent(event)
+	return event
+}
+export function trigger__custom_event(node, event_name, detail = {}) {
+	let event
+	if (window.CustomEvent) {
+		event = new CustomEvent(event_name, { detail })
+	} else {
+		event = document.createEvent('CustomEvent')
+		event.initCustomEvent(event_name, true, true, detail)
+	}
+	node.dispatchEvent(event)
+	return event
+}
