3e521c5d394fe381abc856a86ec699a7d403759f
Brian Takita <brian.takita@gmail.com>
2019-08-12 09:30:23 -0400

    fix: @ctx-core/auth0-ui: If__Loggedin__Auth0: flash of loggedout screen after submitting a successful login credentials

--- packages/auth0-ui/If__Loggedin__Auth0.svelte
+++ packages/auth0-ui/If__Loggedin__Auth0.svelte
@@ -1,10 +1,13 @@
 <script>
-import { __is__loggedin__auth0 } from '@ctx-core/auth0/store'
+import {
+	__is__loggedin__auth0,
+	__is__loggedout__auth0,
+} from '@ctx-core/auth0/store'
 import Auth0 from './Auth0.svelte'
 </script>
 
 {#if $__is__loggedin__auth0}
 	<slot name="loggedin"><slot></slot></slot>
-{:else}
+{:else if $__is__loggedout__auth0}
 	<slot name="loggedout"></slot>
 {/if}
