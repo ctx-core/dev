bdbf347f728d92e52c2d5fa6f9aad43ecc7aaae5
Brian Takita <brian.takita@gmail.com>
2019-09-07 03:34:27 -0400

    feat: @ctx-core/cli-args: _h1__flag__h0__param_name
    feat: @ctx-core/cli-args: _h1__param_name__h0__a1__param_value
    
    feat: @ctx-core/cli-args: _h__param__pick__default,_h__param
    
    BREAKING CHANGE: @ctx-core/cli-args: remove _h__param__pick

--- packages/cli-args/src/lib/index.ts
+++ packages/cli-args/src/lib/index.ts
@@ -1,8 +1,10 @@
 import { some, every, find, each, reduce, } from '@ctx-core/array'
-import { keys, pick } from '@ctx-core/object'
-import { or__in } from '@ctx-core/function'
+import { keys, pick, assign } from '@ctx-core/object'
 const flag_regex = /^--?(.*)/
-const regex__split__dfn__flag = /\s*,\s*/
+const regex__split__param_dfn = /\s*,\s*/
+function _a1__flag(param_dfn) {
+	return param_dfn.split(regex__split__param_dfn)
+}
 export function _h__flag(a1__arg) {
 	const h__flag = {}
 	let i = 0
@@ -30,35 +32,34 @@ export function _a1__arg__h__flag(h__flag, a1__arg) {
 	return reduce(
 		keys(h__flag),
 		(memo, flag)=>{
-			if (flag in h__flag) {
-				memo.push(flag)
-				const _value = h__flag[flag]
-				const value =
-					typeof _value === 'function'
-					? _value(a1__arg)
-					: _value
-				if (value != null) {
-					memo.push(value)
-				}
+			memo.push(flag)
+			const _value = h__flag[flag]
+			const value =
+				typeof _value === 'function'
+				? _value(a1__arg)
+				: _value
+			if (value != null) {
+				memo.push(value)
 			}
 			return memo
 		},
 		[]
 	)
 }
-function _a1__flag(dfn__flag) {
-  return dfn__flag.split(regex__split__dfn__flag)
-}
-export function _a1__arg__default(a1__arg, h1__dfn__flag__h0__value = {}, a1__cancel = []) {
+export function _a1__arg__default(
+	a1__arg,
+	h1__param_dfn__h0__value = {},
+	a1__cancel = []
+) {
 	const a1__arg__default = a1__arg.slice(0)
 	const h__flag = _h__flag(a1__arg__default)
 	if (some(keys(h__flag), flag=>a1__cancel.indexOf(flag) > -1)) {
 		return a1__arg__default
 	}
-	for (let dfn__flag in h1__dfn__flag__h0__value) {
-		const a1__flag = _a1__flag(dfn__flag)
+	for (let param_dfn in h1__param_dfn__h0__value) {
+		const a1__flag = _a1__flag(param_dfn)
 		if (every(a1__flag, flag=>!(flag in h__flag))) {
-			const value = h1__dfn__flag__h0__value[dfn__flag]
+			const value = h1__param_dfn__h0__value[param_dfn]
 			const value__ = typeof value === 'function' ? value() : value
 			const flag =
 				find(a1__flag, flag=>/^--/.test(flag))
@@ -68,18 +69,18 @@ export function _a1__arg__default(a1__arg, h1__dfn__flag__h0__value = {}, a1__ca
 	}
 	return a1__arg__default
 }
-function _h__flag__dfn(a1__dfn__flag) {
+function _h__flag__dfn(a1__param_dfn) {
 	const h__flag__dfn = {}
-	each(a1__dfn__flag, dfn__flag=>{
-		const a1__flag = _a1__flag(dfn__flag)
+	each(a1__param_dfn, param_dfn=>{
+		const a1__flag = _a1__flag(param_dfn)
 		each(a1__flag, flag=>{
 			h__flag__dfn[flag] = a1__flag
 		})
 	})
 	return h__flag__dfn
 }
-export function _h__flag__pick(a1__arg:[], ...a1__dfn__flag) {
-	const h__flag__dfn = _h__flag__dfn(a1__dfn__flag)
+export function _h__flag__pick(a1__arg:[], ...a1__param_dfn) {
+	const h__flag__dfn = _h__flag__dfn(a1__param_dfn)
 	const h__flag = _h__flag(a1__arg)
 	const a1__flag__pick = []
 	each(keys(h__flag), flag=>{
@@ -89,19 +90,67 @@ export function _h__flag__pick(a1__arg:[], ...a1__dfn__flag) {
 	})
 	return pick(h__flag, ...a1__flag__pick)
 }
-export function pick__a1__arg(a1__arg, ...a1__dfn__flag) {
-	const h__flag__pick = _h__flag__pick(a1__arg, ...a1__dfn__flag)
+export function pick__a1__arg(a1__arg, ...a1__param_dfn) {
+	const h__flag__pick = _h__flag__pick(a1__arg, ...a1__param_dfn)
 	return _a1__arg__h__flag(h__flag__pick, a1__arg)
 }
-export function _h__param__pick(a1__arg:[], h__param__dfn__flag) {
-	const h__flag = _h__flag(a1__arg)
-	const a1__key = keys(h__param__dfn__flag)
-	const h__param = {}
-	each(a1__key, key__param => {
-		const dfn__flag = h__param__dfn__flag[key__param]
-		const a1__flag = _a1__flag(dfn__flag)
-		const flag = or__in(h__flag, a1__flag)
-		h__param[key__param] = h__flag[flag]
-	})
+function _h1__flag__h0__param_name(h1__param_name__h0__param_dfn) {
+	const h1__flag__h0__param_name = {}
+	for (let param_name in h1__param_name__h0__param_dfn) {
+		const param_dfn = h1__param_name__h0__param_dfn[param_name]
+		const a1__flag = _a1__flag(param_dfn)
+		each(a1__flag,
+			flag=>
+				h1__flag__h0__param_name[flag] = param_name)
+	}
+	return h1__flag__h0__param_name
+}
+export function _h1__param_name__h0__a1__param_value(a1__arg, h1__param_name__h0__param_dfn) {
+	const h1__param_name__h0__a1__param_value = {}
+	const h1__flag__h0__param_name =
+		_h1__flag__h0__param_name(h1__param_name__h0__param_dfn)
+	let i = 0
+	while (i < a1__arg.length) {
+		const flag = a1__arg[i]
+		const match = flag_regex.test(flag)
+		let j = i + 1
+		if (match) {
+			const param_name = h1__flag__h0__param_name[flag] || flag
+			const a1__value =
+				h1__param_name__h0__a1__param_value[param_name] ||
+				(h1__param_name__h0__a1__param_value[param_name] = [])
+			while (j < a1__arg.length && !flag_regex.test(a1__arg[j])) {
+				a1__value.push(a1__arg[j])
+				j += 1
+			}
+		}
+		i = j
+	}
+	return h1__param_name__h0__a1__param_value
+}
+export function _h__param__pick__default(
+	a1__arg:[],
+	h1__param_name__h0__param_dfn:any,
+	h1__param_name__h0__default_value = {}
+) {
+	const h__param =
+		_h1__param_name__h0__a1__param_value(a1__arg, h1__param_name__h0__param_dfn)
+	const h__param__default = {}
+	for (let param_name in h1__param_name__h0__default_value) {
+		if (h__param[param_name] == null) {
+			const default_value = h1__param_name__h0__default_value[param_name]
+			const default_value__ =
+				typeof default_value === 'function'
+				? default_value(h__param, param_name)
+				: default_value
+			h__param__default[param_name] = default_value__
+		}
+	}
+	assign(h__param, h__param__default)
+	if (h1__param_name__h0__param_dfn) {
+		const a1__param_name = keys(h1__param_name__h0__param_dfn)
+		return pick(h__param, ...a1__param_name)
+	}
 	return h__param
 }
+export const _h__param = _h__param__pick__default
