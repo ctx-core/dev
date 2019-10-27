1e18fd57ff1be57be64f084f40220a2afb11d4f3
Brian Takita <brian.takita@gmail.com>
2019-10-27 02:49:36 -0400

    @ctx-core/core-js,@ctx-core/papaparse: -@ctx-core/logger

--- packages/core-js/env.js
+++ packages/core-js/env.js
@@ -1,5 +1,2 @@
 import { _URL__SHIM__CORE_JS } from './lib'
-import { log, debug } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/core-js/env'
-log(logPrefix)
 export const URL__SHIM__CORE_JS = _URL__SHIM__CORE_JS()
--- packages/papaparse/env.js
+++ packages/papaparse/env.js
@@ -1,7 +1,4 @@
 import { _version__papaparse } from './lib'
-import { log, debug } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/papaparse/env.js'
-log(logPrefix)
 export const PAPAPARSE__URL =
 	process.env.PAPAPARSE__URL
 	|| `https://cdnjs.cloudflare.com/ajax/libs/PapaParse/${_version__papaparse()}/papaparse.js`
