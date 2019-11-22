a93a5d084e9b8d3715a6301d2d48f7273c52194e
Brian Takita <brian.takita@gmail.com>
2019-11-22 16:46:19 -0500

    feat: @ctx-core/chain: typescript

---
+++ packages/chain/lib.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @module @ctx-core/chain/lib
+ */
+export declare function __chain(ctx: any, or: any): (...keys: any[]) => any;
+export declare function __chain__or__fn(ctx: any, fn?: () => void): (...keys: any[]) => any;
+export declare function __chain__or__a(ctx: any, a?: any[]): (...keys: any[]) => any;
+export declare function _chain(ctx: any, ...keys: any[]): any;
