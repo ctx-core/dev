d3ec999ba24990c66e342bbd1927e2e1d111ca78
Brian Takita <brian.takita@gmail.com>
2019-11-23 01:19:49 -0500

    feat: @ctx-core/touch: typescript

---
+++ packages/touch/dom.d.ts
@@ -0,0 +1,3 @@
+export declare function mount__no_touch(): void;
+export declare function enable__scrolling__touch(ctx: any): void;
+export declare function disable__scrolling__touch(ctx: any): void;
--- packages/touch/dom.ts
+++ packages/touch/dom.ts
@@ -1,5 +1,5 @@
 import { add__class } from '@ctx-core/dom'
-import { log, debug } from '@ctx-core/logger'
+import { log } from '@ctx-core/logger'
 const logPrefix = '@ctx-core/touch/dom.js'
 export function mount__no_touch() {
 	log(`${logPrefix}|mount__no_touch`)
