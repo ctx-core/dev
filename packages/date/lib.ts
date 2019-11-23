3b508e728631340a6dc892edfd2c14334ae54073
Brian Takita <brian.takita@gmail.com>
2019-11-22 19:57:03 -0500

    fix: @ctx-core/date: optional date argument

--- packages/date/lib.ts
+++ packages/date/lib.ts
@@ -30,71 +30,71 @@ export const minute = 1000 * 60
 export const hour = 60 * minute
 export const day = 24 * hour
 export const week = 7 * day
-export function _yyyymmddhhmmss(date) {
+export function _yyyymmddhhmmss(date?) {
 	date = date || new Date()
 	return _yyyymmdd(date) +
 		pad2(date.getHours()) +
 		pad2(date.getMinutes()) +
 		pad2(date.getSeconds())
 }
-export function _yyyymmdd(date) {
+export function _yyyymmdd(date?) {
 	date = date || new Date()
 	return date.getFullYear() +
 		pad2(date.getMonth() + 1) +
 		pad2(date.getDate())
 }
-export function _yyyymmddhhmmss__utc(date) {
+export function _yyyymmddhhmmss__utc(date?) {
 	date = date || new Date()
 	return _yyyymmdd__utc(date) +
 		pad2(date.getUTCHours()) +
 		pad2(date.getUTCMinutes()) +
 		pad2(date.getUTCSeconds())
 }
-export function _yyyymmdd__utc(date) {
+export function _yyyymmdd__utc(date?) {
 	date = date || new Date()
 	return date.getUTCFullYear() +
 		pad2(date.getUTCMonth() + 1) +
 		pad2(date.getUTCDate())
 }
-export function _mm_yyyy(date) {
+export function _mm_yyyy(date?) {
 	date = date || new Date()
 	return `${pad2(date.getMonth() + 1)}/${date.getFullYear()}`
 }
-export function _mm_yyyy__utc(date) {
+export function _mm_yyyy__utc(date?) {
 	date = date || new Date()
 	return `${pad2(date.getUTCMonth() + 1)}/${date.getUTCFullYear()}`
 }
-export function _MM_yyyy(date) {
+export function _MM_yyyy(date?) {
 	date = date || new Date()
 	return `${a1__month[date.getMonth()]} ${date.getFullYear()}`
 }
-export function _MM_yyyy__utc(date) {
+export function _MM_yyyy__utc(date?) {
 	date = date || new Date()
 	return `${a1__month[date.getUTCMonth()]} ${date.getUTCFullYear()}`
 }
-export function _M_yyyy(date) {
+export function _M_yyyy(date?) {
 	date = date || new Date()
 	return `${a1__month__abbrev[date.getMonth()]} ${date.getFullYear()}`
 }
-export function _M_yyyy__utc(date) {
+export function _M_yyyy__utc(date?) {
 	date = date || new Date()
 	return `${a1__month__abbrev[date.getUTCMonth()]} ${date.getUTCFullYear()}`
 }
-export function _m_yy(date) {
+export function _m_yy(date?) {
 	date = date || new Date()
 	return `${date.getMonth() + 1}/${date.getFullYear().toString().slice(2)}`
 }
-export function _m_yy__utc(date) {
+export function _m_yy__utc(date?) {
 	date = date || new Date()
 	return `${date.getUTCMonth() + 1}/${date.getUTCFullYear().toString().slice(2)}`
 }
-export function format__date__prose(date) {
+export function format__date__prose(date?) {
 	date = date || new Date()
 	return `${a1__month[date.getMonth()]} ` +
 		`${pad2(date.getDate())}, ` +
 		date.getFullYear()
 }
-export function format__date__prose__utc(date) {
+export function format__date__prose__utc(date?) {
 	date = date || new Date()
 	return `${a1__month[date.getUTCMonth()]} ` +
 		`${pad2(date.getUTCDate())}, ` +
