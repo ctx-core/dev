f536937c7912a31ed4c04e59d82e5465b00f408a
Brian Takita <brian.takita@gmail.com>
2019-11-22 20:58:15 -0500

    feat: @ctx-core/instagram_cache: typescript
    	∋ "jsdom": "^15.2.1"
    	↑ "@aws-sdk/client-dynamodb-node": "^0.1.0-preview.9"
    	↑ "@aws-sdk/client-s3-node": "^0.1.0-preview.7"

---
+++ packages/instagram_cache/fetch.d.ts
@@ -0,0 +1,9 @@
+declare type Opts__fetch__a1__pathname__medium = {
+    Bucket?: string;
+    KEY__A1__PATHNAME__MEDIUM?: string;
+};
+export declare function fetch__a1__pathname__medium(opts?: Opts__fetch__a1__pathname__medium): any;
+export declare const fetch__arr__pathname__medium: typeof fetch__a1__pathname__medium;
+export declare function _a1__pathname__medium(): Promise<any>;
+export declare const _arr__pathname__medium: typeof _a1__pathname__medium;
+export {};
---
+++ packages/instagram_cache/lib.d.ts
@@ -0,0 +1,10 @@
+import 'chromedriver';
+declare type Opts__a1__href__medium = {
+    INSTAGRAM_NAME?: string;
+    reload?: boolean;
+};
+export declare function _a1__href__medium(opts?: Opts__a1__href__medium): Promise<any>;
+export declare const _arr__href__medium: typeof _a1__href__medium;
+export declare function put__cache__scrape__webdriver(options?: {}): Promise<void>;
+export declare function _created_time(medium: any): number;
+export {};
