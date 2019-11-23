758115c75965e3bec961b3e9ddd30312e646a89a
Brian Takita <brian.takita@gmail.com>
2019-11-22 19:37:56 -0500

    feat: @ctx-core/highlight.js: typescript
    fix: @ctx-core/highlight.js: project description: highlight.js instead of google
    
    @ctx-core/graphql-service: -rollup.config.js

---
+++ packages/highlight.js/lib.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @typedef HighlightJS
+ */
+/**
+ * Calls `hljs.initHighlighting` in a DOM environment with dynamically loaded content.
+ * @param {HighlightJS} hljs
+ */
+export declare function refresh__initHighlighting(hljs: any): void;
