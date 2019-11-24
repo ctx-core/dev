7e1b3a627e8c371ea311f28e04abd89560b3d72b
Brian Takita <brian.takita@gmail.com>
2019-11-24 13:11:16 -0500

    fix: @ctx-core/google: argument signature

--- packages/google/lib.ts
+++ packages/google/lib.ts
@@ -1,10 +1,10 @@
 import { log } from '@ctx-core/logger'
 const logPrefix = '@ctx-core/google/lib.js'
 declare const ga
-export function push__dataLayer() {
+export function push__dataLayer(obj, ...arg_a1:any[]) {
 	log(`${logPrefix}|push__dataLayer`)
 	const dataLayer = window['dataLayer'] = window['dataLayer'] || []
-	dataLayer.push(...arguments)
+	dataLayer.push(obj, ...arg_a1)
 }
 export function ga$() {
 	log(`${logPrefix}|ga$`)
