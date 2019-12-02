1c108eddc515ccbaa11268b93ef80504ff90f2bb
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:20:05 -0500

    @ctx-core/sapper: git submodule

--- packages/sapper/store.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const __page__sapper: import("svelte/store").Writable<any>;
-export declare const __preloading__sapper: import("svelte/store").Writable<any>;
-export declare const __session__sapper: import("svelte/store").Writable<any>;
-export declare const __path__sapper: import("svelte/store").Readable<any>;
-export declare function touch__session(session: any): void;
