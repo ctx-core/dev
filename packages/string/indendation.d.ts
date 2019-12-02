a08774201341526c0b9969af49377381954d667e
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:21:31 -0500

    @ctx-core/string: git submodule

--- packages/string/indendation.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function _indentation(count__indent: any, indent?: string): string;
-export declare const $indentation: typeof _indentation;
-export declare function _regexp__indentation(spaces: any): RegExp;
-export declare const $regexp__indentation: typeof _regexp__indentation;
--- packages/string/indendation.ts
+++
@@ -1,9 +0,0 @@
-export function _indentation(count__indent, indent='	') {
-	return new Array(count__indent + 1).join(indent)
-}
-export const $indentation = _indentation
-export function _regexp__indentation(spaces) {
-	const regexpSource = '^' + _indentation(spaces)
-	return new RegExp(regexpSource, 'gm')
-}
-export const $regexp__indentation = _regexp__indentation
