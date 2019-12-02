0ea0eceec334d947385d3fcf74b387fcbcdd31bf
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:11:22 -0500

    @ctx-core/cache: git submodule

--- packages/cache/store.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function _reload__store__cache(store: any): () => void;
-export declare function _ensure__store__cache(__store: any, query: any): (ctx__query: any, id: any) => Promise<any>;
