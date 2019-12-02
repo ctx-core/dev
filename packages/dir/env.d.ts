f26839db7f90a19cdbe4af35b32b8885a5724402
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:13:11 -0500

    @ctx-core/dir: git submodule

--- packages/dir/env.d.ts
+++
@@ -1,2 +0,0 @@
-import '@ctx-core/env/env';
-export declare const TMP_DIR: string;
--- packages/dir/env.ts
+++
@@ -1,2 +0,0 @@
-import '@ctx-core/env/env'
-export const TMP_DIR = process.env.TMP_DIR || `${process.env.PWD}/tmp`
