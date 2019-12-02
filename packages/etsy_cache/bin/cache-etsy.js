a95c452d51519951ac3e02eb20262d431df03158
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:14:18 -0500

    @ctx-core/etsy_cache: git submodule

--- packages/etsy_cache/bin/cache-etsy.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-require = require('esm')(module)
-const { put__cache__etsy } = require('../s3')
-put__cache__etsy()
--- packages/etsy_cache/lib.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function _a1__listing__etsy(): Promise<any>;
-export declare const _arr__listing__etsy: typeof _a1__listing__etsy;
-export declare function _a1__images__listing__etsy(a1__listing__etsy: any): Promise<any[]>;
-export declare const _arr__images__listing__etsy: typeof _a1__images__listing__etsy;
