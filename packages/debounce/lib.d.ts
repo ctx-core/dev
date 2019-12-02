12bfec1549eae9c95eeba416596b261ccfd87d6a
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:12:59 -0500

    @ctx-core/debounce: git submodule

--- packages/debounce/lib.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function debounce(ctx: any, ...a1__opt: any[]): Promise<any>;
-export declare function ensure__table__debounce(ctx: any): any;
-export declare function assign__finish__debounce(ctx: any, key: any): any;
-export declare function finish__debounce(ctx: any, key: any): any;
--- packages/debounce/store.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const __table__debounce: import("svelte/store").Writable<{}>;
-export declare function debounce(opts: any): Promise<any>;
