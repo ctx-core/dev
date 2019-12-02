6d55b105a2f84376a8edba1d7c1f29ef1f3f6d66
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:10:46 -0500

    @ctx-core/auth0-management: git submodule

--- packages/auth0-management/lib.d.ts
+++
@@ -1,7 +0,0 @@
-declare type Params__management__auth0 = {
-    domain?: string;
-    clientId?: string;
-    clientSecret?: string;
-};
-export declare function _management__auth0(params?: Params__management__auth0): any;
-export {};
