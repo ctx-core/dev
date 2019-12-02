593d50df67e69151337953135d991c5c700f8f3c
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:16:41 -0500

    @ctx-core/isomorphic-fetch: git submodule

--- packages/isomorphic-fetch/lib.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
-export default _default;
-export declare const Headers: Headers;
-export declare const Request: Request;
-export declare const Response: Response;
--- packages/isomorphic-fetch/lib.ts
+++
@@ -1,9 +0,0 @@
-import { _no__dom } from '@ctx-core/dom'
-export default (
-	_no__dom()
-	? require('isomorphic-fetch')
-	: window.fetch
-) as (input:RequestInfo, init?:RequestInit)=>Promise<Response>
-export const Headers:Headers = _no__dom() ? require('isomorphic-fetch').Headers : window.Headers
-export const Request:Request = _no__dom() ? require('isomorphic-fetch').Request : window.Request
-export const Response:Response = _no__dom() ? require('isomorphic-fetch').Response : window.Response
