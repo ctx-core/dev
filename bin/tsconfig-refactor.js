11e698709c1ccccfdbc509d55d92b44952e71756
Brian Takita <brian.takita@gmail.com>
2019-12-02 16:57:15 -0500

    fix: bin/tsconfig-refactor.js: iterating over tsconfig.json files
    ∋ "esm": "^3.2.25"

--- bin/tsconfig-refactor.js
+++ bin/tsconfig-refactor.js
@@ -1,21 +1,30 @@
-#!/usr/bin/env
-import fs from 'fs'
-import { promisify } from 'util'
+#!/usr/bin/env node
+require = require('esm')(module)
+const fs = require('fs')
+const { promisify } = require('util')
 const { clone } = require('@ctx-core/object')
-const { map } = require('@ctx-core/array')
+const { map, _present__a1 } = require('@ctx-core/array')
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
-import glob from 'glob'
+const glob = promisify(require('glob'))
 main()
 async function main() {
-	const tsconfig__base = JSON.parse(await readFile('./tsconfig.json'))
-  const promise_a1 = map(glob('packages/*/tsconfig.json', async tsconfig_path => {
-		let tsconfig = JSON.parse(await readFile(tsconfig_path))
+	const tsconfig__base = JSON.parse((await readFile('./tsconfig.json')).toString())
+	const promise_a1 = map(await glob('packages/*/tsconfig.json'), async (tsconfig_path, arg2) => {
+		let tsconfig = JSON.parse((await readFile(tsconfig_path)).toString())
+		let update
 		if (tsconfig.extends == '../../tsconfig.json') {
+			update = true
 			delete tsconfig.extends
 			tsconfig = clone(tsconfig__base, tsconfig)
+		}
+		if (_present__a1(tsconfig.references)) {
+			update = true
+			tsconfig.references = []
+		}
+		if (update) {
 			await writeFile(tsconfig_path, JSON.stringify(tsconfig, null, '\t'))
 		}
-	}))
+	})
 	await Promise.all(promise_a1)
 }
