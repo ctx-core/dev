8ab2684eb12887bd349556de14c1164bfc813a35
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:20:30 -0500

    @ctx-core/scroll: git submodule

--- packages/scroll/lib.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function _is__visible(el: any): boolean;
-export declare function _is__visible__(top: any, bottom: any, innerHeight: any): boolean;
-export declare function _is__active(el: any): boolean;
-export declare function _is__active__(top: any, bottom: any): boolean;
--- packages/scroll/store.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const __active__Sticky__Scroll: import("svelte/store").Writable<{}>;
-export declare function add__active__Sticky__Scroll(key: any): void;
-export declare function remove__active__Sticky__Scroll(key: any): void;
-export declare function _active__active__Sticky__Scroll(key: any): any;
-export declare function _match__active__Sticky__Scroll(key: any, active: any): boolean;
