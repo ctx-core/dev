69a92ddeba033d46f0f40676f31dc0cbec4faf67
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:12:53 -0500

    @ctx-core/date: git submodule

--- packages/date/Date.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-import { format__date__prose } from './lib'
-export let date
-</script>
-
-{format__date__prose(date)}
--- packages/date/Date__Local.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-import { _date__append__local_tz } from './lib'
-import Date from './Date.svelte'
-export let date
-</script>
-
-<Date date="{_date__append__local_tz(date)}"></Date>
