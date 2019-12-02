26f780ab19fa20063098eb53a68a35e237f9b234
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:18:13 -0500

    @ctx-core/nav: git submodule

--- packages/nav/store.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const __opened__nav: import("svelte/store").Writable<boolean>;
-export declare function toggle__nav(): void;
-export declare function open__nav(): void;
-export declare function close__nav(): void;
