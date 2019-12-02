4f9cddbcaca8fc0e8679ccc15f98a887ad216580
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:16:29 -0500

    @ctx-core/instagram: git submodule

--- packages/instagram/fetch.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function _medium(pathname: any): Promise<any>;
-export declare function fetch__medium(pathname: any): Promise<any>;
--- packages/instagram/fetch.ts
+++
@@ -1,8 +0,0 @@
-import { fetch } from '@ctx-core/fetch'
-export async function _medium(pathname) {
-	const response = await fetch__medium(pathname)
-	return response.json()
-}
-export async function fetch__medium(pathname) {
-	return fetch(`https://api.instagram.com/oembed/?url=http://instagr.am${pathname}`)
-}
