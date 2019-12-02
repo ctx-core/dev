3b19fa9f5bec4c98da1ddf05a03312b556af910d
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:23:05 -0500

    @ctx-core/uikit: git submodule

--- packages/uikit/lib.d.ts
+++
@@ -1,6 +0,0 @@
-import UIkit from 'uikit/src/js/uikit-core';
-export { UIkit };
-declare type Opts__register__component = {
-    force?: boolean;
-};
-export declare function register__component(name: any, component: any, opts?: Opts__register__component): void;
