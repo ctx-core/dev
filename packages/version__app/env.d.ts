dc3384af54ae93621a4310f839e955e41ea3ff72
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:23:30 -0500

    @ctx-core/version__app: git submodule

--- packages/version__app/env.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const RELEASE_VERSION: string;
-export declare const SOURCE_VERSION: string;
-export declare const CACHE_VERSION: string;
--- packages/version__app/store.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const __version__app: import("svelte/store").Readable<string | number>;
-export declare const __query__version__app: import("svelte/store").Readable<string>;
