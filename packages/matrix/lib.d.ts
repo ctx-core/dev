3ad69358ad4883c1fe95f35577627e377fb9fbde
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:17:55 -0500

    @ctx-core/matrix: git submodule

--- packages/matrix/lib.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function normalize(matrix: any, length__columns?: number): any[];
-export declare const normalize__row__major__matrix: typeof normalize;
-export declare function dot(...matrices: any[]): number;
-export declare function dotMultiply(...matrices: any[]): any[];
-export declare function length__validate(arrays: any): any;
-export declare function multiply__scalar__matrix(scalar: any, matrix: any): any[];
-export declare function _matrix__nansum__columns(matrix: any, length__columns: any): any[];
-export declare function _unit__matrix(length: number): any[];
