8185da80a806163a19011a1bf901f9494a4156ad
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:12:35 -0500

    @ctx-core/csv: git submodule

--- packages/csv/lib.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Opts__transform__table__csv = {
-    _cell?: (value: any, column: number, row: number) => any;
-};
-export declare function transform__table__csv(csv?: string, opts?: Opts__transform__table__csv): any[];
-export declare function cast__rows(rows: any, columns: any): void;
-export declare function push__row_id__i(rows: any, columns: any): void;
-export declare function toLowerCase__column_name(csv: any): string;
-export declare const toLowerCase__column_name__csv: typeof toLowerCase__column_name;
-export {};
