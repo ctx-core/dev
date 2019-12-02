113c382b8793270dd1970863a71ac5b3c396a657
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:18:31 -0500

    @ctx-core/package: git submodule

--- packages/package/lib.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function _version__package(path__package: any): any;
-export declare const _version: typeof _version__package;
-export declare function verify__version__node(path__package?: any): void;
-export declare function _version__node(path__package?: any): any;
-export declare function _json__package(path__package?: any): any;
