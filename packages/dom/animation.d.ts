d6189ee9f96c06546daf2fd8e3c98946a2881038
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:13:29 -0500

    @ctx-core/dom: git submodule

--- packages/dom/animation.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @param scrollTargetY
- * @param speed
- * @param easing
- * @see {@link https://stackoverflow.com/questions/12199363/scrollto-with-animation#answer-26798337}
- */
-export declare function scrollToY(scrollTargetY: any, speed: any, easing: any): void;
