14c858d0d5ed3e63011127f8eefef317eb776950
Brian Takita <brian.takita@gmail.com>
2019-12-01 19:23:32 -0500

    feat: @ctx-core/sapper-amp: new package

---
+++ packages/sapper-amp/lib.d.ts
@@ -0,0 +1,11 @@
+import { Request } from 'polka';
+export declare type Opts__render = {
+    req: Request;
+    component: any;
+};
+export declare type Opts__fn__html__amp = {
+    Layout: any;
+    Error: any;
+    CONTEXT_KEY: any;
+};
+export declare function _fn__html__amp(opts: Opts__fn__html__amp): ({ req, component, }: Opts__render) => any;
