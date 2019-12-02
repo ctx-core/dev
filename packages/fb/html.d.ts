4368b4840cddb65caffbdce69b6fae6b85b5ddc4
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:14:30 -0500

    @ctx-core/fb: git submodule

--- packages/fb/html.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function _attrs__html__fb(): {
-    xmlns: string;
-    'xmlns:og': string;
-    'xmlns:fb': string;
-};
--- packages/fb/html.ts
+++
@@ -1,7 +0,0 @@
-export function _attrs__html__fb() {
-	return {
-		xmlns: 'http://www.w3.org/1999/xhtml',
-		'xmlns:og': 'http://ogp.me/ns#',
-		'xmlns:fb': 'https://www.facebook.com/2008/fbml'
-	}
-}
