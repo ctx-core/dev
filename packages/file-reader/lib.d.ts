81d8b31d204ceadc0c4277e07ab79bb40077ba09
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:14:48 -0500

    @ctx-core/file-reader: git submodule

--- packages/file-reader/lib.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function readAsDataURL(file: any): Promise<unknown>;
-export declare function readAsText(file: any): Promise<unknown>;
-export declare function readAsArrayBuffer(file: any): Promise<unknown>;
