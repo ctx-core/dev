9c3c3b1fb275b9cb2b592e63d559b7825223f68b
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:19:22 -0500

    @ctx-core/random: git submodule

--- packages/random/lib.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function _float__random__uniform(min?: number, max?: number): number;
-export declare function _float__random__normal(min?: number, max?: number): number;
-export declare function _float__random__distribution(min: number, max: number, _distribution: any): number;
-export declare function _int__random__uniform(min?: number, max?: number): number;
-export declare function _int__random__normal(min?: number, max?: number): number;
-export declare function _int__random__distribution(min: number, max: number, $distribution: any): number;
-export declare function _random__uniform(): number;
-export declare function _random__normal(): number;
