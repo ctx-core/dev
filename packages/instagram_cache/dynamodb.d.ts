c89b91461633a986d090f5be5e7b06a18fc6f3d7
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:16:35 -0500

    @ctx-core/instagram_cache: git submodule

--- packages/instagram_cache/dynamodb.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function put_all_media(): Promise<any[]>;
-export declare function _Item(pathname: any): Promise<{
-    pathname: any;
-    created_time: number;
-    info: {};
-}>;
--- packages/instagram_cache/s3.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Opts__a1__pathname__medium = {
-    region?: string;
-    KEY__A1__PATHNAME__MEDIUM?: string;
-};
-export declare function get__a1__pathname__medium(opts?: Opts__a1__pathname__medium): Promise<any>;
-export declare const get__arr__pathname__medium: typeof get__a1__pathname__medium;
-export declare function put__a1__pathname__medium(a1__pathname__medium: any, opts?: Opts__a1__pathname__medium): Promise<import("@aws-sdk/client-s3-node/types/PutObjectOutput").PutObjectOutput>;
-export declare const put__arr__pathname__medium: typeof put__a1__pathname__medium;
-export {};
