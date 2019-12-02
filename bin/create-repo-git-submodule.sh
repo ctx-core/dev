f66f7676bcf1304bbf1d3e2ae6a45708c164e456
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:05:48 -0500

    .envrc: + .dotenv
    + bin/create-repo-git-submodule.sh

---
+++ bin/create-repo-git-submodule.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+NAME=$1
+curl -H "Authorization: bearer $GITHUB_SECRET" https://api.github.com/orgs/ctx-core/repos -d "{\"name\": \"$NAME\"}"
+mv "packages/$NAME" "packages/$NAME"_
+pushd "packages/$NAME"_
+git init .
+git add .
+git commit -m 'new repo'
+git remote add origin "git@github.com:ctx-core/$NAME.git"
+git push -u origin master
+popd
+rm -rf "packages/$NAME"_
+git add .
+git submodule add "https://github.com/ctx-core/$NAME" "packages/$NAME"
