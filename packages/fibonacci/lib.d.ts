62280bef67107a2a96668004c9f2ae6df3a10eb0
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:14:42 -0500

    @ctx-core/fibonacci: git submodule

--- packages/fibonacci/lib.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function _number__fibonacci(n: any): any;
--- packages/fibonacci/lib.ts
+++
@@ -1,9 +0,0 @@
-export function _number__fibonacci(n) {
-	return (
-		n < 1
-		? 0
-		: n <= 2
-			? 1
-			: _number__fibonacci(n - 1) + _number__fibonacci(n - 2)
-	)
-}
