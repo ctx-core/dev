fa52959e04924529541d2e72594182f3c91d1219
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:17:30 -0500

    @ctx-core/local-storage: git submodule

--- packages/local-storage/lib.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function load__ctx__localStorage(): any;
-export declare function assign__ctx__localStorage(): any;
-export declare function set__ctx__localStorage(ctx: any): any;
-export declare function remove__ctx__localStorage(...args: any[]): any;
-export declare function _ctx__set__json__from__localStorage(scope__: any): {};
-export declare function sync__json__localStorage(key: any, value: any): any;
-export declare const store__json__localStorage: typeof sync__json__localStorage;
-export declare function sync__localStorage(key: any, value: any): any;
-export declare const store__localStorage: typeof sync__localStorage;
