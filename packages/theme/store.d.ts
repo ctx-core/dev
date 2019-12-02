2a343ab5db3f571fcfd09d56debf2767a43f4e72
Brian Takita <brian.takita@gmail.com>
2019-12-02 12:22:28 -0500

    @ctx-core/theme: git submodule

--- packages/theme/store.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const __theme__invert: import("svelte/store").Writable<any>;
-export declare function invert__theme(): void;
-export declare const __class__theme__invert: import("svelte/store").Readable<string>;
