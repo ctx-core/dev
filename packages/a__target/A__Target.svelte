e114b5f6cd25feeed45316009ae16b0cf4480d26
Brian Takita <brian.takita@gmail.com>
2019-12-02 00:30:01 -0500

    feat: @ctx-core/a__target: target: _new instead of _blank

--- packages/a__target/A__Target.svelte
+++ packages/a__target/A__Target.svelte
@@ -4,7 +4,7 @@ export let target = null
 export let alt = ''
 export let title = ''
 let target__
-$: target__ = target || href.split(':').length > 1 ? '_blank' : '_self'
+$: target__ = target || href.split(':').length > 1 ? '_new' : '_self'
 </script>
 
 <a
