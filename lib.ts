45830ea4e63935c444f9ffadb61432a919e24bc7
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:16:21 -0500

    Squashed 'packages/pipe/' content from commit a2258359f
    git-subtree-dir: packages/pipe
    git-subtree-split: a2258359fb61ce6fff498fbd8823b29f7ffc930d

---
+++ lib.ts
@@ -0,0 +1,26 @@
+import { Socket } from 'net'
+export function _is__piped() {
+	return Socket === process.stdin.constructor
+}
+export async function _txt__piped():Promise<string> {
+	let txt__file__piped = ''
+	return new Promise(resolve => {
+		if (_is__piped()) {
+			process.stdin.on('readable', () => {
+				let chunk
+				while ((chunk = process.stdin.read()) !== null) {
+					txt__file__piped += chunk
+				}
+			})
+			process.stdin.on('end', () => {
+				resolve(txt__file__piped.trim())
+			})
+		} else {
+			resolve('')
+		}
+	})
+}
+export async function _a1__piped() {
+  const txt__piped = await _txt__piped()
+	return txt__piped == null ? txt__piped : txt__piped.split('\n')
+}
