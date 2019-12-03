8215d6805d0392f6e717abc711b787248142c0bf
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:16:24 -0500

    Squashed 'packages/polarity/' content from commit 2dca8a1b5
    git-subtree-dir: packages/polarity
    git-subtree-split: 2dca8a1b5b25a0635eeb1132415835f2eceeac80

---
+++ Icon__Polarity.svelte
@@ -0,0 +1,25 @@
+<script>
+export let polarity = 0
+</script>
+
+<span class="Icon_Polarity {$$props.class||''}">
+	{#if polarity > 0}
+		<span class="positive">&uarr;</span>
+	{:else if polarity < 0}
+		<span class="negative">&darr;</span>
+	{:else}
+		<span class="neutral">&larr;</span>
+	{/if}
+</span>
+
+<style type="text/css">
+.positive {
+	color: #33A532;
+}
+.neutral {
+	color: #999999;
+}
+.negative {
+	color: #CC0605;
+}
+</style>
---
+++ lib.d.ts
@@ -0,0 +1,1 @@
+export declare function _text__polarity(value: any): string;
