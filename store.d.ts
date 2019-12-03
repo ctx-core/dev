903d997286b66550ae8027a1eba6bea349e2ac20
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:13:47 -0500

    Squashed 'packages/csv/' content from commit e5036cc74
    git-subtree-dir: packages/csv
    git-subtree-split: e5036cc74994a25f3b89c92bac3da651f4ad3e70

---
+++ store.d.ts
@@ -0,0 +1,9 @@
+export declare const __path__csv: import("svelte/store").Writable<any>;
+declare type Opts__load__data__csv = {
+    path__csv?: string;
+    table?: [][];
+    domain__table?: [][];
+    domain__ticks?: [];
+};
+export declare function load__data__csv(opts?: Opts__load__data__csv): Promise<unknown>;
+export {};
