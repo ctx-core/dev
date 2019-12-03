886a42c7807ef2ac252417f6170c43ea34aae51b
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:13:55 -0500

    Squashed 'packages/date/' content from commit 1f65daa1d
    git-subtree-dir: packages/date
    git-subtree-split: 1f65daa1d53edd8111e322a0940b7aced36a1cb5

---
+++ Date.svelte
@@ -0,0 +1,6 @@
+<script>
+import { format__date__prose } from './lib'
+export let date
+</script>
+
+{format__date__prose(date)}
---
+++ Date__Local.svelte
@@ -0,0 +1,7 @@
+<script>
+import { _date__append__local_tz } from './lib'
+import Date from './Date.svelte'
+export let date
+</script>
+
+<Date date="{_date__append__local_tz(date)}"></Date>
