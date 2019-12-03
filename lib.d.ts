16aa70557b5ff423cadc5d33313a57b46e7bed7d
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:14:22 -0500

    Squashed 'packages/esm/' content from commit 738eb143d
    git-subtree-dir: packages/esm
    git-subtree-split: 738eb143db0e9b626cb0a35b18ab3413e7750f27

---
+++ lib.d.ts
@@ -0,0 +1,3 @@
+declare const _esm: any;
+declare function _require__esm(params?: {}): any;
+declare function _require__esm__cjs(): any;
---
+++ lib.ts
@@ -0,0 +1,11 @@
+const _esm = require('esm')
+module.exports = {
+	_require__esm,
+	_require__esm__cjs,
+}
+function _require__esm(params={}) {
+  return _esm(module, params)
+}
+function _require__esm__cjs() {
+  return _require__esm({ cjs: true })
+}
