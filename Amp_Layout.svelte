fd22fff06a15918ca7c0e7c4dc149b199342a8f3
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:16:54 -0500

    Squashed 'packages/sapper-amp/' content from commit 06e77b759
    git-subtree-dir: packages/sapper-amp
    git-subtree-split: 06e77b759429b482448341e27baf6fa5bc446614

---
+++ Amp_Layout.svelte
@@ -0,0 +1,69 @@
+<svelte:head>
+	<script async src="https://cdn.ampproject.org/v0.js"></script>
+	<script
+		async
+		custom-element="amp-form"
+		src="https://cdn.ampproject.org/v0/amp-form-0.1.js"
+	></script>
+	<noscript>
+		<style amp-boilerplate>
+			body {
+				-webkit-animation: none;
+				-moz-animation: none;
+				-ms-animation: none;
+				animation: none;
+			}
+		</style>
+	</noscript>
+</svelte:head>
+
+<slot></slot>
+
+<style amp-boilerplate>
+:global(body) {
+	-webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+	-moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+	-ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+	animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+}
+@-webkit-keyframes -amp-start {
+	from {
+		visibility: hidden;
+	}
+	to {
+		visibility: visible;
+	}
+}
+@-moz-keyframes -amp-start {
+	from {
+		visibility: hidden;
+	}
+	to {
+		visibility: visible;
+	}
+}
+@-ms-keyframes -amp-start {
+	from {
+		visibility: hidden;
+	}
+	to {
+		visibility: visible;
+	}
+}
+@-o-keyframes -amp-start {
+	from {
+		visibility: hidden;
+	}
+	to {
+		visibility: visible;
+	}
+}
+@keyframes -amp-start {
+	from {
+		visibility: hidden;
+	}
+	to {
+		visibility: visible;
+	}
+}
+</style>
