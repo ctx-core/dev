29570f5267ccea8a04e4421556adc677e09c7ffd
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:16:46 -0500

    Squashed 'packages/route/' content from commit ff739690c
    git-subtree-dir: packages/route
    git-subtree-split: ff739690c10bb7b6224bb905e9e1bcf63209ac74

---
+++ store.ts
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store'
+// @ts-ignore
+import { subscribe__debug } from '@ctx-core/store'
+export const __route = writable('')
+export const __query__route = writable({})
