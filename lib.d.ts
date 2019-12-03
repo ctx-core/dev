f307965bd4f091ea43d3c8d6afbb91bc63a4bc2b
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:15:35 -0500

    Squashed 'packages/jwt/' content from commit 2bb60234c
    git-subtree-dir: packages/jwt
    git-subtree-split: 2bb60234cfe597358c528aea4a6703f0aa743030

---
+++ lib.d.ts
@@ -0,0 +1,7 @@
+export declare type Token = {
+    error?: any;
+    id_token?: string;
+};
+export declare function _token__jwt__authorization__header(authorization: any): any;
+export declare function validate__current__jwt(token__jwt: any): void;
+export declare function _exp__token__jwt(token__jwt: any): any;
