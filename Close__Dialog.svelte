2b0d87bfd0ded0a83ba7d9630b20954e5c3b8278
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:14:00 -0500

    Squashed 'packages/dialog/' content from commit d20fde37e
    git-subtree-dir: packages/dialog
    git-subtree-split: d20fde37ee996061b9dcf327f4b6ee22ecad3002

---
+++ Close__Dialog.svelte
@@ -0,0 +1,16 @@
+<script>
+export let tabindex = '0'
+</script>
+
+<a
+	href="."
+	class="close Close__Dialog {$$props.class||''}"
+	tabindex="{tabindex || '0'}"
+	on:click|preventDefault
+><slot>&#10006;</slot></a>
+
+<style type="text/scss">
+:global(.Close__Dialog) {
+	text-decoration: none;
+}
+</style>
