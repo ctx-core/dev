6befd9cf5adc9ea04bb05dd8e95055fac914981a
Brian Takita <brian.takita@gmail.com>
2019-12-03 01:14:59 -0500

    Squashed 'packages/google/' content from commit d59e360ec
    git-subtree-dir: packages/google
    git-subtree-split: d59e360ec857f10abb52f33e186dfb1acf46e302

---
+++ GTM__Head.svelte
@@ -0,0 +1,7 @@
+<script>
+import { onMount } from 'svelte'
+import { init__dataLayer } from './dom'
+onMount(init__dataLayer)
+</script>
+
+<slot></slot>
---
+++ lib.d.ts
@@ -0,0 +1,2 @@
+export declare function push__dataLayer(obj: any, ...arg_a1: any[]): void;
+export declare function ga$(): void;
