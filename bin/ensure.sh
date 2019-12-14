6094995e54d498db13759b77b2499c3ed23e5b6c
Brian Takita <brian.takita@gmail.com>
2019-12-14 04:00:24 -0500

    bin/ensure.sh moved to @ctx-core/monorepo

--- bin/ensure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-function ensure {
-	PS="$(ps aux | grep -- "$(echo $@)" | grep -v grep)"
-	if [ -z "$PS" ]; then
-		echo "$@"
-		exec "$@"
-	fi
-}
-[ "$(basename $0)" = "ensure.sh" ] && ensure $@
