2e3d5731b94ffdceaef75a2284b6a73b0866b7c0
Brian Takita <brian.takita@gmail.com>
2020-09-20 04:27:17 -0700

    @ctx-core/package-tools: package_refactor: enforce tab indentation on package.json

--- tools/package-tools/package_refactor.js
+++ tools/package-tools/package_refactor.js
@@ -55,6 +55,9 @@ export async function package_refactor() {
             update = true;
             delete pkg.gitHead;
         }
+        if (/\n  /.test(package_json)) {
+            update = true;
+        }
         if (update) {
             console.debug(replacement, {
                 repository: pkg.repository,
