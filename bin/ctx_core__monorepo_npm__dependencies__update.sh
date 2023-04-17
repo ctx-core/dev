f691927e061313c046af7bc3040b1b02127c8019
Brian Takita <brian.takita@gmail.com>
2023-04-17 13:44:38 -0400

    ctx-core-update-npm-packages=>ctx_core__monorepo_npm__dependencies__update - update-npm-packages

--- bin/ctx_core__monorepo_npm__dependencies__update.sh
+++ bin/ctx_core__monorepo_npm__dependencies__update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$(dirname $0)"
 TEMP=$(mktemp)
-monorepo-npm-check-updates -o $TEMP
+monorepo_npm__dependencies__update -o $TEMP
 UPDATES=$(cat "$TEMP" | grep -v '@ctx-core/ctx-core ' | grep -Fv '. ')
 echo "$UPDATES"
 echo "$UPDATES" | package-manifest-changeset
