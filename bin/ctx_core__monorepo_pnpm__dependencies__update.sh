c52b0aabf8442ec34377466bb24d1a4c4e19bd78
Brian Takita <brian.takita@gmail.com>
2023-11-14 05:21:06 -0500

    ctx-core: 2.2.0
    update dependencies

--- bin/ctx_core__monorepo_pnpm__dependencies__update.sh
+++ bin/ctx_core__monorepo_pnpm__dependencies__update.sh
@@ -2,6 +2,6 @@
 DIR="$(dirname $0)"
 TEMP=$(mktemp)
 monorepo_pnpm__dependencies__update -o $TEMP
-UPDATES=$(cat "$TEMP" | grep -v '@ctx-core/ctx-core ' | grep -Fv '. ')
+UPDATES=$(cat "$TEMP" | grep -v '@ctx-core/dev ' | grep -Fv '. ')
 echo "$UPDATES"
 echo "$UPDATES" | package-manifest-changeset
